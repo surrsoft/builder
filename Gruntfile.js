09ebd3f9317b3d5c6a1cd41dd039b489be6a1995
Oleg Elifantiev <oleg@elifantiev.ru>
2013-06-03 14:08:13 +0400

    Сборщик умеет параметризироваться через командную строку

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+   var path = require('path');
+   var target = path.resolve(grunt.option('root'));
+   var app = grunt.option('application');
+   var configBuilder = require('./lib/config-builder.js');
+
+   target = path.resolve(target) || '';
+
+   process.env.WS = path.join(target, app, 'ws');
+
+   grunt.option('color', !!process.stdout.isTTY);
+
+   grunt.loadNpmTasks('grunt-packer');
+   grunt.loadNpmTasks('grunt-contrib-copy');
+   grunt.loadNpmTasks('grunt-wsmod-packer');
+
+   grunt.file.setBase(target);
+
+   grunt.initConfig(configBuilder(app));
+
+   grunt.registerTask('default', ['packwsmod', 'packjs', 'packcss']);
+
+};
