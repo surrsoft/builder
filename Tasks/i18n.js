7a215126e287ca71db64ddc6d24d33c713689777
Shilovda <sceletron@gmail.com>
2014-01-24 00:33:29 +0400

    Сделал в сборщике подготовку к локализации для xhtml

--- Tasks/i18n.js
+++ Tasks/i18n.js
@@ -1,11 +1,17 @@
-var helpers = require('../lib/i18n.js');
+var helpers = require('../lib/i18n.js'),
+    helpers2 = require('../lib/i18n-prepare.js');
 
 module.exports = function(grunt) {
 
    grunt.registerMultiTask('i18n', 'Translate static', function() {
       var needSplit = grunt.option('translate'),
           indexDict = grunt.option('index-dict'),
+          prepare = grunt.option('prepare'),
           application = this.data.application;
+
+      if (prepare) {
+         helpers2.prepareXHTML(grunt, application);
+      }
 
       if (needSplit || indexDict) {
          helpers.findDictionary(grunt, this.data.dict || ['**/resources/lang/*.json']);
