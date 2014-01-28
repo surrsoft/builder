5ccf1e814ea3322c8be46c467a52bd10d09165b3
Oleg Elifantiev <oleg@elifantiev.ru>
2014-01-28 18:59:21 +0400

    Убрал все идентификаторы через подчеркивание...

--- Tasks/json-generation.js
+++ Tasks/json-generation.js
@@ -1,5 +1,5 @@
 var
-  sbis3_json_generator = require('../node_modules/sbis3-json-generator'),
+  sbis3JSONGenerator = require('sbis3-json-generator'),
   path = require('path');
 
 module.exports = function(grunt) {
@@ -16,14 +16,14 @@ module.exports = function(grunt) {
 
     var relative = path.relative.bind(path, [process.cwd()]);
 
-    var generator_options = {
+    var generatorOptions = {
       outDir: options.outDir && relative(options.outDir),
       isWs: options.isWs,
       showStdout: false
     };
 
 
-    sbis3_json_generator.generateJson(relative(options.inDir), done, generator_options);
+    sbis3JSONGenerator.generateJson(relative(options.inDir), done, generatorOptions);
   });
 
 };
