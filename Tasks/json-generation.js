feaa42441ed171ff88dba9d0207812f36e05ab48
Shilovda <sceletron@gmail.com>
2014-02-26 15:32:14 +0400

    Ускоряем сборку дистрибутива с локализацией: 1 - логирование времени выполнения задач 2 - убрал флаг --translate, так как разбивать на статики уже не надо 3 - --index-dict теперь принимает языки, вместо --translate

--- Tasks/json-generation.js
+++ Tasks/json-generation.js
@@ -1,29 +1,32 @@
-var
-  sbis3JSONGenerator = require('sbis3-json-generator'),
-  path = require('path');
+var sbis3JSONGenerator = require('sbis3-json-generator'),
+    path = require('path');
 
 module.exports = function(grunt) {
 
-  grunt.registerTask('generateJson', 'Generate json', function() {
+   grunt.registerTask('generateJson', 'Generate json', function() {
 
-    var done = this.async();
+      var done = this.async();
 
-    var options = this.options({
+      var options = this.options({
          'inDir': grunt.option('root'),
          'outDir': '',
          'isWs': false
       });
 
-    var relative = path.relative.bind(path, [process.cwd()]);
+      var relative = path.relative.bind(path, [process.cwd()]);
 
-    var generatorOptions = {
-      outDir: options.outDir && relative(options.outDir),
-      isWs: options.isWs,
-      showStdout: false
-    };
+      var generatorOptions = {
+         outDir: options.outDir && relative(options.outDir),
+         isWs: options.isWs,
+         showStdout: false
+      };
 
 
-    sbis3JSONGenerator.generateJson(relative(options.inDir), done, generatorOptions);
-  });
+      grunt.log.ok(grunt.template.today('hh:MM:ss') + ': Построение метаинформации начато.');
+
+      sbis3JSONGenerator.generateJson(relative(options.inDir), done, generatorOptions);
+
+      grunt.log.ok(grunt.template.today('hh:MM:ss') + ': Построение метаинформации выполнено.');
+   });
 
 };
