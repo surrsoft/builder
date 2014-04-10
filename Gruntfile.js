6a9c6bc171d09d94317da9b462b66221387000b2
Oleg Elifantiev <oleg@elifantiev.ru>
2014-04-10 17:09:11 +0400

    При запуске вывожу версию в stdout

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,4 +36,6 @@ module.exports = function(grunt) {
       grunt.registerTask('default', defaultTasks);
    }
 
+   grunt.log.ok('SBIS3 Builder v' + require(path.join(__dirname, 'package.json')).version);
+
 };
