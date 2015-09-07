121f09379c7226a45c5dbe3e496aff69a97809aa
ai.esin <ai.esin@tensor.ru>
2015-09-07 15:55:13 +0300

    Исправления

--- Tasks/xhtmlmin.js
+++ Tasks/xhtmlmin.js
@@ -2,30 +2,28 @@ var fs = require('fs');
 var path = require('path');
 module.exports = function(grunt) {
    grunt.registerMultiTask('xhtmlmin', 'minify xhtml', function () {
-      var done = this.async(),
+      var done = this.async,
           files = grunt.file.expand({cwd: process.cwd()}, this.data);
-
       files.forEach(function(file) {
-         readFileData(file, function(fileData, originalFile) {
-            var fileDir = path.dirname(originalFile),
-                fileName = path.basename(originalFile);
+         readFileData(file, function(fileData, originalFile, done) {
             fileData = fileData.replace(/\s{2,}/g,' ');
             fileData = fileData.replace(/ </g,'<');
-            fs.writeFile(fileDir + '/' + fileName, fileData, function(err) {
+            fs.writeFile(originalFile, fileData, function(err) {
                if(err) {
                   grunt.log.error(err);
                } else {
-                  grunt.log.ok(fileName);
+                  grunt.log.ok(path.basename(originalFile));
                }
             });
-         });
-      });
+         }, done);
+      }, done);
+      done();
    });
 
-   function readFileData(file, callback) {
+   function readFileData(file, callback, done) {
       fs.readFile(file, {encoding: 'utf8'}, function (err, data) {
       if (err) throw err;
-      callback(data, file);
+      callback(data, file, done);
       });
    }
 };
