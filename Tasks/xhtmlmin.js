a46df42144ad0b0bfcf5f77793a1add21209a7fb
ai.esin <ai.esin@tensor.ru>
2015-09-07 14:27:20 +0300

    исправления

--- Tasks/xhtmlmin.js
+++ Tasks/xhtmlmin.js
@@ -2,41 +2,31 @@ var fs = require('fs');
 var path = require('path');
 module.exports = function(grunt) {
    grunt.registerMultiTask('xhtmlmin', 'minify xhtml', function () {
-      var done = this.async();
-      var availableFiles;
-      this.files.forEach(function(file) {
-         availableFiles = getAvailableFiles(file.src);
-      }, this);
-      for(var i=0; i < availableFiles.length; i++) {
-         readFileData(availableFiles[i]);
-      }
+      var done = this.async(),
+          availableFiles,
+          files = grunt.file.expand({cwd: process.cwd()}, this.data);
+
+      files.forEach(function(file) {
+         readFileData(file, function(fileData, originalFile) {
+            var fileDir = path.dirname(originalFile),
+                fileName = path.basename(originalFile);
+            fileData = fileData.replace(/\s{2,}/g,' ');
+            fileData = fileData.replace(/ </g,'<');
+            fs.writeFile(fileDir + '/' + fileName, fileData, function(err) {
+               if(err) {
+                  grunt.log.error(err);
+               } else {
+                  grunt.log.ok(fileName);
+               }
+            });
+         });
+      });
    });
 
-   var getAvailableFiles = function(filesArray) {
-      return filesArray.filter(function (filepath) {
-      if(!grunt.file.exists(filepath)) {
-         return false;
-      }
-      return true;
-      });
-   };
-
-   var readFileData = function(file) {
+   function readFileData(file, callback) {
       fs.readFile(file, {encoding: 'utf8'}, function (err, data) {
       if (err) throw err;
-      minifyDoc(data, file);
+      callback(data, file);
       });
-   };
-
-   var minifyDoc = function(fileData, originalFile) {
-      var fileDir = path.dirname(originalFile),
-          fileName = path.basename(originalFile);
-      fileData = fileData.replace(/\s{2,}/g,' ');
-      fileData = fileData.replace(/ </g,'<');
-      fs.writeFile(fileDir + '/' + fileName, fileData, function(err) {
-         if(err) {
-            console.log(err);
-         }
-      });
-   };
+   }
 };
