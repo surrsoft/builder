c226690ad56abccdcf487ebef1256f513bc73bf2
ai.esin <ai.esin@tensor.ru>
2015-09-14 12:52:23 +0300

    Чтение и запись файлов осуществляется с помощью grunt api

--- Tasks/xhtmlmin.js
+++ Tasks/xhtmlmin.js
@@ -1,29 +1,19 @@
-var fs = require('fs');
-var path = require('path');
 module.exports = function(grunt) {
-   grunt.registerMultiTask('xhtmlmin', 'minify xhtml', function () {
-      var done = this.async,
-          files = grunt.file.expand({cwd: process.cwd()}, this.data);
+   grunt.registerMultiTask('xhtmlmin', 'minify xhtml and html', function () {
+      grunt.log.ok(grunt.template.today('hh:MM:ss') + ': Запускается задача xhtmlmin.');
+      var files = grunt.file.expand({cwd: process.cwd()}, this.data);
       files.forEach(function(file) {
          readFileData(file, function(fileData, originalFile) {
             fileData = fileData.replace(/\s{2,}/g,' ');
             fileData = fileData.replace(/ </g,'<');
-            fs.writeFile(originalFile, fileData, function(err) {
-               if(err) {
-                  grunt.log.error(err);
-               } else {
-                  grunt.log.ok(path.basename(originalFile));
-               }
-            });
+            grunt.file.write(originalFile, fileData);
          });
       });
-      done();
+      grunt.log.ok(grunt.template.today('hh:MM:ss')+ ': Задача xhtmlmin выполнена.');
    });
 
    function readFileData(file, callback) {
-      fs.readFile(file, {encoding: 'utf8'}, function (err, data) {
-      if (err) throw err;
+      var data = grunt.file.read(file, {encoding: 'utf8'});
       callback(data, file);
-      });
    }
 };
