6e9c1b63082acbf75b2900e42df99dc5dc3c5502
Shilovda <sceletron@gmail.com>
2015-09-28 11:47:41 +0300

    Поддержка нового механизма сбора зависимостей и упаковки собственных зависимостей

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
    var target = path.resolve(grunt.option('root') || '');
    var app = grunt.option('application') || '';
    var configBuilder = require('./lib/config-builder.js');
-   var defaultTasks = ['collect-dependencies', 'packwsmod', 'cssmin', 'uglify'];
+   var defaultTasks = ['packwsmod', 'cssmin', 'uglify'];
 
    if (doConcat === true || doConcat === undefined) {
       defaultTasks.push('packjs', 'packcss');
