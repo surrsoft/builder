8175dba32f0c186dd05f1cc7f33f9f5562397649
ai.esin <ai.esin@tensor.ru>
2015-11-30 12:00:01 +0300

    Убираю все комментарии, включая скриптовые. Убираю все переводы строк

--- Tasks/xhtmlmin.js
+++ Tasks/xhtmlmin.js
@@ -1,26 +1,74 @@
 module.exports = function(grunt) {
     function minifyFile(data) {
-        var scriptWord = '<script',
-            endScriptWord = '/script>',
-            startScr = data.indexOf(scriptWord),
-            endScr,
-            out = '';
-        while(startScr != -1) {
-            startScr += scriptWord.length;
-            out += min(data.substr(0, startScr));
-            data = data.substr(startScr, data.length);
-            endScr = data.indexOf(endScriptWord);
-            endScr += endScriptWord.length;
-            out += data.substr(0, endScr);
-            data = data.substr(endScr, data.length);
-            startScr = data.indexOf(scriptWord);
-        }
-        out += (data.substr(0, data.length));
-        return out;
+            var scriptWord = '<script',
+                endScriptWord = '/script>',
+                startScr = data.indexOf(scriptWord),
+                endScr,
+                out = '',
+                allowSlahes = [];
+            while(startScr != -1) {
+                startScr += scriptWord.length;
+                out += min(data.substr(0, startScr));
+                data = data.substr(startScr, data.length);
+                endScr = data.indexOf(endScriptWord);
+                endScr += endScriptWord.length;
+                allowSlahes = saveSlahes(data.substr(0, endScr));
+                out += miniScript(data.substr(0, endScr), allowSlahes);
+                data = data.substr(endScr, data.length);
+                startScr = data.indexOf(scriptWord);
+            }
+            out += (data.substr(0, data.length));
+            return out;
+    }
+
+    function saveSlahes(data) {
+        //Пройдет по скрипту и сохранит все вхождения двойных слешей, заключенных в кавычки
+        var correctDoubleSlashes = /".*(\/{2}.*?".*?\r\n)/g,
+            correctSlashes = /'.*(\/{2}.*?'.*?\r\n)/g,
+            savedSlahes = [];
+        data.replace(correctDoubleSlashes, function(str, p1) {
+            savedSlahes.push(p1);
+            return str;
+        });
+        data.replace(correctSlashes, function(str, p1) {
+            savedSlahes.push(p1);
+            return str;
+        });
+        return savedSlahes;
+    }
+
+    function miniScript(data, slahes) {
+        /* Найдет все вхождения двойных слешей и сравнит с сохраненным массивом
+        * Если найденное вхождение совпадет с одним из сохраненных, то пропустим его.
+        * Остальные вхождения примем за комментарий и удалим. Так же удалим многострочные комментарии и переводы строк*/
+        var possibleComments = /\/\/.*\r\n/g,
+            mnComments = /\s?\/\*[\s\S]*?\*+\/+?\s?/g;
+        data = data.replace(possibleComments, function(str) {
+            var isNotComment;
+            slahes.forEach(function(item) {
+                if(str.indexOf(item) != -1) {
+                    isNotComment = true;
+                }
+            });
+            if(isNotComment) {
+                return str;
+            } else {
+                return '';
+            }
+        });
+        data = data.replace(mnComments, '');
+        data = data.replace(/\s{2,}/g,' ');
+        return data;
     }
 
     function min(data) {
-        data = data.replace(/<!--[\s\S]*?-->/g, '');
+        data = data.replace(/<!--[\s\S]*?-->/g, function(str) {
+            if(str.indexOf('[if') != -1) {
+                return str;
+            } else {
+                return ''
+            }
+        });
         data = data.replace(/\s{2,}/g,' ');
         data = data.replace(/ </g,'<');
         return data;
