50c80d17c0f6348d1b001bf0f91f9f102ba890f8
Shilovda <sceletron@gmail.com>
2016-01-25 14:44:37 +0300

    Структурировал

--- Tasks/i18n.js
+++ Tasks/i18n.js
@@ -1,14 +1,12 @@
-var indexDict = require('../lib/i18n.js').indexDict,
-    prepareXHTML = require('../lib/i18n-prepare.js').prepareXHTML,
-    createResultDict = require('../lib/i18n-dict.js').createResultDict,
-    packageDictionary = require('../lib/i18n-packer.js').packageDictionary;
+var indexDict = require('../lib/i18n/indexDictionary').indexDict,
+    prepareXHTML = require('../lib/i18n/prepareXHTML').prepareXHTML,
+    createResultDict = require('../lib/i18n/createResultDictionary').createResultDict;
 
 module.exports = function(grunt) {
 
    grunt.registerMultiTask('i18n', 'Translate static', function() {
       var languages = grunt.option('index-dict'),
           prepare = grunt.option('prepare-xhtml'),
-          packer = grunt.option('package'),
           makeDict = grunt.option('make-dict'),
           application = this.data.application;
 
@@ -24,10 +22,6 @@ module.exports = function(grunt) {
 
       if (languages) {
          indexDict(grunt, languages, this.data.dict, application);
-      }
-
-      if (packer) {
-         packageDictionary(grunt, this.data, application);
       }
 
       grunt.log.ok(grunt.template.today('hh:MM:ss')+ ': Задача i18n выполнена.');
