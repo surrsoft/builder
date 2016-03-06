c760a5b9f15a5ee267a9db2cb2efa01d69dd842a
shilovda <shilovda@tensor.ru>
2016-03-06 14:42:13 +0300

    Отдельный флаг для генерации json

--- Tasks/i18n.js
+++ Tasks/i18n.js
@@ -1,7 +1,8 @@
 var indexDict = require('../lib/i18n/indexDictionary').indexDict,
     prepareXHTML = require('../lib/i18n/prepareXHTML').prepareXHTML,
     createResultDict = require('../lib/i18n/createResultDictionary').createResultDict,
-    packageDictionary = require('../lib/i18n/packer.js').packageDictionary;
+    packageDictionary = require('../lib/i18n/packer').packageDictionary,
+    jsonGenerator = require('../lib/i18n/jsonGenerator').jsonGenerator;
 
 module.exports = function(grunt) {
 
@@ -9,6 +10,8 @@ module.exports = function(grunt) {
       grunt.log.ok(grunt.template.today('hh:MM:ss') + ': Запускается задача i18n.');
 
       var taskDone = this.async();
+
+      grunt.option('json-generate') && jsonGenerator(grunt, done);
 
       grunt.option('make-dict') && createResultDict(grunt, done);
 
