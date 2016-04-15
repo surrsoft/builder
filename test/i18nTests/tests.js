7096de9ea4c80ab6a84c1087050b7241c15df459
ai.esin <ai.esin@tensor.ru>
2016-04-15 16:35:59 +0300

    правки путей

--- test/i18nTests/tests.js
+++ test/i18nTests/tests.js
@@ -42,7 +42,7 @@ describe('i18n Indexing Test', function() {
 describe('i18n Packing Test ', function() {
 
    before(function(done){
-      exec('grunt --root=./test/fixture --package', {
+      exec('grunt i18n --root=./test/fixture --package', {
          cwd: path.join(__dirname, '../../')
       }, function() {
          done();
@@ -50,6 +50,7 @@ describe('i18n Packing Test ', function() {
    });
 
    it('package', function(){
+
       var IndexDict = readJSON('./../fixture/resources/Shop/Index/resources/lang/en-US/en-US.json'),
          HeadDict = readJSON('./../fixture/resources/Shop/Head/resources/lang/en-US/en-US.json'),
          Contents = readJSON('./../fixture/resources/contents.json'),
@@ -77,13 +78,12 @@ describe('i18n Packing Test ', function() {
        }
 
    });
-
 });
 
 describe('i18n prepareXHTML Test ', function() {
 
-   var modules = "C:/Users/ai.esin/Repo/builder/sbis3-builder/test/i18nTests/res/modules.json",
-      cache = 'C:/Users/ai.esin/Repo/builder/sbis3-builder/test/i18nTests/res';
+   var modules = path.join(__dirname, 'res/modules.json'),
+      cache = path.join(__dirname, 'res/cache');
    before(function(done){
       var globalPaths = [
          path.join(__dirname, './../fixture/resources/Shop/Head'),
@@ -102,17 +102,19 @@ describe('i18n prepareXHTML Test ', function() {
          IndexXhtmlContent = fs.readFileSync(path.join(__dirname, './../fixture/resources/Shop/Index/Index.xhtml')).toString();
       assert(HeadXhtmlContent.indexOf('{[Перейти на главную страницу]}') > -1, 'Head.xhtml: Phrase "Перейти на главную страницу" is not covered');
       assert(IndexXhtmlContent.indexOf('{[Добро пожаловать в интернет-магазин "Тензор"!]}') > -1, 'Index.xhtml: Phrase "Добро пожаловать в интернет-магазин "Тензор"!" is not covered');
-      assert(IndexXhtmlContent.indexOf('<option name="tooltip" value="{[Заголовок]}"></option>') > -1, 'Index.xhtml: Option "Заголовок" is not covered')
+      assert(IndexXhtmlContent.indexOf('{[Заголовок]}') > -1, 'Index.xhtml: option "Заголовок" is not covered')
    });
 
 });
 
 describe('i18n resultDictionary Test ', function() {
 
-   var modules = "C:/Users/ai.esin/Repo/builder/sbis3-builder/test/i18nTests/res/modules.json",
-      cache = 'C:/Users/ai.esin/Repo/builder/sbis3-builder/test/i18nTests/res',
-      out = cache + '/out.json';
+   var modules = path.join(__dirname, 'res/modules.json'),
+      cache = path.join(__dirname, 'res/cache'),
+      out = path.join(cache, 'out.json'),
+      modulesPaths;
    before(function(done){
+      modulesPaths = JSON.parse(fs.readFileSync(modules, 'utf-8'));
       exec('grunt --root=./test/fixture --out=' + out + ' --modules='+modules + ' --json-cache=' + cache + ' --make-dict', {
          cwd: path.join(__dirname, '../../')
       }, function() {
@@ -121,12 +123,14 @@ describe('i18n resultDictionary Test ', function() {
    });
 
    it('make-dict', function(){
-      var outJsonContent = readJSON('./res/out.json');
+      var outJsonContent = readJSON('./res/cache/out.json');
 
       assert.equal(outJsonContent[0].key, 'Перейти на главную страницу', 'Field key in the result dictionary has wrong value');
-      assert.equal(outJsonContent[0].module, 'resources\\Shop\\Head\\Head.xhtml', 'Field module in the result dictionary has wrong value');
-      assert.equal(outJsonContent[1].key, 'Добро пожаловать в интернет-магазин "Тензор"!', 'Field key in the result dictionary has wrong value');
-      assert.equal(outJsonContent[1].module, 'resources\\Shop\\Index\\Index.xhtml', 'Field module in the result dictionary has wrong value');
+      assert.equal(outJsonContent[0].module, modulesPaths[0] + '\\Head.xhtml', 'Field module in the result dictionary has wrong value');
+      assert.equal(outJsonContent[1].key, 'Заголовок', 'Field key in the result dictionary has wrong value');
+      assert.equal(outJsonContent[1].module, modulesPaths[1] + '\\Index.xhtml', 'Field module in the result dictionary has wrong value');
+      assert.equal(outJsonContent[2].key, 'Добро пожаловать в интернет-магазин "Тензор"!', 'Field key in the result dictionary has wrong value');
+      assert.equal(outJsonContent[2].module, modulesPaths[1] + '\\Index.xhtml', 'Field module in the result dictionary has wrong value');
    });
 
 });
