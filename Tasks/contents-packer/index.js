42d247109a098fc703d1a550b2c4168d795ee755
aa.oborevich <aa.oborevich@tensor.ru>
2016-04-22 12:04:45 +0300

    рефакторинг

--- Tasks/contents-packer/index.js
+++ Tasks/contents-packer/index.js
@@ -5,16 +5,12 @@
 'use strict';
 
 var
-   packerModule = require('./packer'),
-   serviceRoot, contents, dependencies, packer;
+   packerModule = require('./packer');
 
 
 
 module.exports = function(options, cb) {
-   serviceRoot = options.root;
-   contents = options.contents;
-   dependencies = options.deps;
-   packer = new packerModule(serviceRoot, contents, dependencies);
+   var packer = new packerModule(options.root, options.contents, options.deps);
 
    packer.run(cb);
 };
