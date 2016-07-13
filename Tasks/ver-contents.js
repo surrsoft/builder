e751dc0e7dd90db30c59295cea79b257aa480fa9
aa.egorov <aa.egorov@tensor.ru>
2016-07-13 16:48:00 +0300

    Добавляю buildnumber в contents.js(json)

---
+++ Tasks/ver-contents.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = function (grunt) {
+   var buildnumber = grunt.option('versionize');
+   var root = grunt.option('root') || '';
+   var app = grunt.option('application') || '';
+   var resourcesPath = path.join(root, app, 'resources');
+   grunt.registerMultiTask('ver-contents', 'versionize contents.[js|json]', function () {
+      grunt.log.ok(grunt.template.today('hh:MM:ss') + ': Запускается задача ver-contents.');
+
+      try {
+         var contents = require(path.join(resourcesPath, 'contents.json'));
+         contents.buildnumber = buildnumber;
+         grunt.file.write(path.join(resourcesPath, 'contents.json'), JSON.stringify(contents, null, 2));
+         grunt.file.write(path.join(resourcesPath, 'contents.js'), 'contents='+JSON.stringify(contents));
+      } catch (err) {
+         grunt.fail.fatal(err);
+      }
+   });
+};
