e0199ca2e0f6b7cc1aeeb4bd7c60c6bc07aed123
aa.egorov <aa.egorov@tensor.ru>
2016-08-15 18:22:12 +0300

    Генерю contents.json(js) если его нет

--- Tasks/ver-contents.js
+++ Tasks/ver-contents.js
@@ -7,11 +7,18 @@ module.exports = function (grunt) {
    var root = grunt.option('root') || '';
    var app = grunt.option('application') || '';
    var resourcesPath = path.join(root, app, 'resources');
+   var contents = {};
+
    grunt.registerMultiTask('ver-contents', 'versionize contents.[js|json]', function () {
       grunt.log.ok(grunt.template.today('hh:MM:ss') + ': Запускается задача ver-contents.');
 
       try {
-         var contents = require(path.join(resourcesPath, 'contents.json'));
+         contents = require(path.join(resourcesPath, 'contents.json'));
+      } catch (err) {
+         grunt.log.warn('contents.json doesn\'t exist');
+      }
+
+      try {
          contents.buildnumber = buildnumber;
          grunt.file.write(path.join(resourcesPath, 'contents.json'), JSON.stringify(contents, null, 2));
          grunt.file.write(path.join(resourcesPath, 'contents.js'), 'contents='+JSON.stringify(contents));
