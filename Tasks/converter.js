7c2af0d3bb26f6384316d32cde9151cf89e13987
aa.egorov <aa.egorov@tensor.ru>
2016-08-15 18:08:55 +0300

    Полная транслитерация

--- Tasks/converter.js
+++ Tasks/converter.js
@@ -68,12 +68,12 @@ module.exports = function (grunt) {
             var ext = path.extname(abspath);
             if (!symlink || (i18n && (ext == '.xhtml' || ext == '.html'))) {
                try {
-                  grunt.file.copy(abspath, path.join(resourcesPath, moduleName, transliterate(path.relative(input, abspath))));
+                  grunt.file.copy(abspath, transliterate(path.join(resourcesPath, moduleName, path.relative(input, abspath))));
                } catch (err) {
                   grunt.log.error(err);
                }
             } else {
-               mkSymlink(abspath, path.join(resourcesPath, moduleName, transliterate(path.relative(input, abspath))));
+               mkSymlink(abspath, transliterate(path.join(resourcesPath, moduleName, path.relative(input, abspath))));
             }
          });
       });
