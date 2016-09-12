7f24059ee2e71b5b841e0b0d3203d0135537ce0c
aa.egorov <aa.egorov@tensor.ru>
2016-09-12 18:36:13 +0300

    Переименовываем все модули тчо можем внутри xhtml и js

--- lib/utils/rebase.py
+++ lib/utils/rebase.py
@@ -6,8 +6,14 @@ from datetime import datetime
 from sys import argv, version_info
 
 define = compile('define\([\'"](js!)?([\.\w/]+)[\'"],\s*(\[[\s\S]*?\])?')
-inputDir = argv[1] or './'
-outputDir = argv[2] or './output'
+require = compile('require\(\s*(\[[\s\S]*?\])')
+other_modules = compile('[\'"]([a-zA-Z-_:!]+)?((?<!\w)SBIS3\.[a-zA-Z0-9/_.]+)[\'"]')
+inputDir = argv[1] if len(argv) > 1 else './'
+outputDir = argv[2] if len(argv) > 2 else './output'
+
+
+def mk_list(st):
+    return list(map(transform_dep, st.replace('[', '').replace(']', '').replace('\'', '').replace('"', '').split(',')))
 
 
 def is_remote(dep):
@@ -27,6 +33,10 @@ def transform_dep(dep):
         return '\'' + '/'.join(parts) + '\''
 
 
+def mod_repl(matchobj):
+    return transform_dep(matchobj.group(0).replace('"', '').replace('\'', ''))
+
+
 start = datetime.now()
 error_count = 0
 done_count = 0
@@ -42,7 +52,7 @@ for root, dirs, files in walk(inputDir):
 
 js_total = len(js_files)
 
-print('Found {0} *.js files in {1} files for {2}'.format(js_total, all_files, datetime.now() - start))
+print('Found {0} *.js files in {1} files for {2}    '.format(js_total, all_files, datetime.now() - start))
 
 for file in js_files:
     name, root = file.split('@')
@@ -55,6 +65,13 @@ for file in js_files:
         text = f.read()
         f.close()
 
+        requirements = require.findall(text)
+        req = requirements[0] if len(requirements) > 0 else None
+        if req:
+            req = mk_list(requirements[0])
+            req = '[' + ', '.join(req) + ']'
+            text = require.sub('require(' + req, text)
+
         defanddeps = define.findall(text)
         if len(defanddeps) > 0:
             mod = defanddeps[0][1].split('.')
@@ -64,9 +81,14 @@ for file in js_files:
 
             deps = defanddeps[0][2]
             if deps:
-                deps = deps.replace('[', '').replace(']', '').replace('\'', '').replace('"', '').split(',')
-                deps = list(map(transform_dep, deps))
+                deps = mk_list(deps)
                 deps = '[' + ', '.join(deps) + ']'
+
+            req = requirements[0] if len(requirements) > 0 else None
+            if req:
+                req = mk_list(requirements[0])
+                req = '[' + ', '.join(req) + ']'
+                text = require.sub('require(' + req, text)
 
             mod.pop(0) if mod[0] == 'SBIS3' or mod[0] == 'SBIS' else None
             oldModuleName = name.split('.')[0]
@@ -85,6 +107,7 @@ for file in js_files:
                 new_file = open(new_dest + '.js', 'wb')
 
             text = define.sub('define(\'' + '/'.join(mod) + '\', ' + deps, text)
+            text = other_modules.sub(mod_repl, text)
             try:
                 new_file.write(text)
 
@@ -119,5 +142,43 @@ for file in js_files:
     if done_count % 1000 == 0:
         print('{0} *.js files processed for {1}'.format(done_count, datetime.now() - start))
 
+# TODO  можно перенести в первый walk()
+print('\nStarting replace for *.xhtml files')
+start_xhtml = datetime.now()
+done_xhtml_count = 0
+for root, dirs, files in walk(outputDir):
+    for name in files:
+        ext = splitext(name)[1]
+        if ext == '.xhtml':
+            try:
+                if version_info >= (3, 0, 0):
+                    f = open(join(root, name), 'r', encoding="utf_8")
+                else:
+                    f = open(join(root, name), 'r')
+                text = f.read()
+                f.close()
+
+                text = other_modules.sub(mod_repl, text)
+
+                if version_info >= (3, 0, 0):
+                    f = open(join(root, name), 'w', encoding="utf_8")
+                else:
+                    f = open(join(root, name), 'wb')
+
+                try:
+                    f.write(text)
+                except UnicodeEncodeError as err:
+                    error_count += 1
+                    print('UnicodeEncodeError: ', join(root, name), err)
+                f.close()
+
+            except UnicodeDecodeError as err:
+                error_count += 1
+                print('UnicodeDecodeError: ', join(root, name), err)
+
+            done_xhtml_count += 1
+            if done_xhtml_count % 1000 == 0:
+                print('{0} *.xhtml files processed for {1}'.format(done_xhtml_count, datetime.now() - start_xhtml))
+
 print('\n\033[92mDone!\033[0m\nTotal time: {0}\nFiles processed: {1}\nErrors: {2}'
-      .format(datetime.now() - start, done_count, error_count))
+      .format(datetime.now() - start, done_count + done_xhtml_count, error_count))
