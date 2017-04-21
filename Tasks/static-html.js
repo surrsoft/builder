3bc8a7c750af46a870299d03486fdff61cffc7d0
is.kudryavcev <is.kudryavcev@tensor.ru>
2017-04-21 13:43:15 +0300

    https://online.sbis.ru/opendoc.html?guid=faa70a96-cbfe-466e-a3aa-78d3305a833e&des=

--- Tasks/static-html.js
+++ Tasks/static-html.js
@@ -43,12 +43,12 @@ module.exports = function (grunt) {
             APPLICATION_ROOT: application,
             SBIS_ROOT: application + 'ws/',
             RESOURCE_ROOT: application + 'resources/',
-            SERVICES_PATH: srvPath || application + 'service/sbis-rpc-service300.dll',
+            SERVICES_PATH: srvPath || application + 'service/',
             USER_PARAMS: userParams,
             GLOBAL_PARAMS: globalParams,
-            SAVE_LAST_STATE: false,
-            ROOT: root,
-            START_DIALOG: ''
+            SAVE_LAST_STATE: false: root,
+            START_DIALOG: '',
+            ROOT
         };
     }
 
