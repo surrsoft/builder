5dfa5527ebf1f7a0805ae8958594cfc2bbc551cd
Колбешин Ф.А <fa.kolbeshin@tensor.ru>
2017-12-23 16:37:43 +0300

    Распараллеливаем таску из за жестоко долгого времени работы в последовательном режиме. https://online.sbis.ru/opendoc.html?guid=724d82fb-7bc4-41ce-a9e8-8bab62c06090

--- Tasks/uglify.js
+++ Tasks/uglify.js
@@ -7,12 +7,13 @@ var
     fs = require('fs'),
     UglifyJS = require('uglify-js'),
     humanize = require('humanize'),
-    path = require('path');
+    path = require('path'),
+    async = require('async');
 
 module.exports = function uglifyJsTask(grunt) {
     grunt.registerMultiTask('uglify', 'Задача минификации JS', function() {
         grunt.log.ok(`${humanize.date('H:i:s')} : Запускается задача uglify.`);
-        const done = this.async();
+        const taskDone = this.async();
         var
             getAvailableFiles = function (filesArray) {
                 return filesArray.filter(function (filepath) {
@@ -30,7 +31,7 @@ module.exports = function uglifyJsTask(grunt) {
              */
             isPresentationService =  grunt.file.exists(path.join(process.env.ROOT, 'resources', 'WS.Core'));
         // Iterate over all src-dest file pairs.
-        this.files.forEach(function (currentFile) {
+        async.eachSeries(this.files, function(currentFile, done) {
             var availableFiles = getAvailableFiles(currentFile.src);
             if (availableFiles.length === 0) {
                 grunt.log.warn('Destination ' + currentFile.dest + ' not written because src files were empty.');
@@ -69,10 +70,19 @@ module.exports = function uglifyJsTask(grunt) {
                 if (isPresentationService) {
                     fs.writeFileSync(sourceMapPath, data.map);
                 }
-                grunt.log.ok(`File ${currentPath} successfully minified`);
             });
+            /**
+             * используем такой способ возвращения callback'а итеративной функции, поскольку
+             * в противном случае мы получим переполнение размера стека вызовов. Подробнее
+             * можно почитать здесь:
+             * https://github.com/caolan/async/blob/master/intro.md#synchronous-iteration-functions
+             */
+            async.setImmediate(function(){
+                done();
+            });
+        }, function() {
+            grunt.log.ok(`${humanize.date('H:i:s')} : Задача uglify выполнена.`);
+            taskDone();
         });
-        grunt.log.ok(`${humanize.date('H:i:s')} : Задача uglify выполнена.`);
-        done();
     });
 };
