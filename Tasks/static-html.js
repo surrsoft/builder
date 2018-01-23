4d4558fe62262792187066a554c68641c7f70c24
aa.shipin <aa.shipin@tensor.ru>
2018-01-23 11:22:42 +0300

    https://online.sbis.ru/opendoc.html?guid=6dd3b0a0-91ac-4600-9e7a-535f1e61fd96

--- Tasks/static-html.js
+++ Tasks/static-html.js
@@ -9,6 +9,9 @@ const helpers = require('./../lib/utils/helpers');
 const humanize = require('humanize');
 const async = require('async');
 
+const tmplLocalizator = require('./../lib/i18n/tmplLocalizator');
+const Deferred = global.requirejs('Core/Deferred');
+
 const dblSlashes = /\\/g;
 
 let cache = {};
@@ -26,6 +29,15 @@ function parseObjectExpression(properties) {
         obj[prop.key.name] = prop.value.value;
     });
     return obj;
+}
+function warnTmplBuild(err, fullPath) {
+    grunt.log.warn(`resources error. An ERROR occurred while building template! ${err.message}, in file: ${fullPath}`);
+}
+function errorTmplBuild(err, fullName, fullPath) {
+    grunt.log.error(`Resources error. An ERROR occurred while building template!
+    ---------File name: ${fullName}
+    ---------File path: ${fullPath}`);
+    grunt.fail.fatal(err);
 }
 
 module.exports = function (grunt) {
@@ -99,6 +111,82 @@ module.exports = function (grunt) {
 
         generateHTML(htmlTemplate, outFileName + '.html', replaceOpts, applicationRoot, cb, inclReplace);
     }
+
+    grunt.registerMultiTask('tmpl-html', 'Generate static html from .html.tmpl files', function () {
+        grunt.log.ok(`${humanize.date('H:i:s')}: Запускается задача tmpl-html.`);
+        let start = Date.now();
+        const
+            done = this.async(),
+            root = this.data.root,
+            application = this.data.application,
+            applicationRoot = path.join(root, application),
+            resourcesRoot = path.join(applicationRoot, 'resources'),
+            fileRegExp = this.data.fileRegExp;
+
+        global.requirejs(['View/Builder/Tmpl', 'View/config', 'View/Runner/tclosure'], function (tmpl, config, tclosure) {
+            helpers.recurse(resourcesRoot, function (fullPath, callback) {
+                if (!fileRegExp.test(fullPath)) {
+                    return;
+                }
+
+                fs.readFile(fullPath, function (err, html) {
+                    if (err) {
+                        grunt.fail.fatal(err);
+                        return callback(err);
+                    }
+
+                    var def = new Deferred();
+                    let conf = {config: config, filename: ''};
+
+                    var templateRender = Object.create(tmpl);
+
+                    if (html.indexOf('define') === 0) {
+                        def.errback(new Error(fullPath + ' - не шаблон!'));
+                        return;
+                    }
+
+                    try {
+                        tmplLocalizator.parseTmpl(grunt, html, fullPath).addCallback(function (traversedObj) {
+                            const traversed = traversedObj.astResult;
+                            try {
+                                let tmplFunc = templateRender.func(traversed, conf);
+                                var resHtml = tmplFunc({}, {}, undefined, false, undefined, tclosure);
+
+                                var newFullPath = fullPath.replace(/\.tmpl$/, '');
+                                // если файл уже есть, удалим
+                                if (fs.existsSync(newFullPath)) {
+                                    fs.unlinkSync(newFullPath);
+                                }
+                                // создадим файл с новым содержимым
+                                fs.writeFileSync(newFullPath, resHtml.toString(), {
+                                    flag: "wx"
+                                });
+                                def.callback();
+                            } catch (err) {
+                                warnTmplBuild(err, fullPath);
+                                def.errback(err);
+                            }
+                        }).addErrback(function (err) {
+                            warnTmplBuild(err, fullPath);
+                            def.errback(err);
+                        });
+                    } catch (err) {
+                        errorTmplBuild(err, fileName, fullPath);
+                        def.errback(err);
+                    }
+                    return def;
+                });
+            }, function (err) {
+                if (err) {
+                    grunt.fail.fatal(err);
+                }
+
+                console.log(`Duration: ${(Date.now() - start) / 1000} sec`);
+
+                done();
+            });
+        });
+    });
 
     grunt.registerMultiTask('static-html', 'Generate static html from modules', function () {
         grunt.log.ok(`${humanize.date('H:i:s')}: Запускается задача static-html.`);
