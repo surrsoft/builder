6e54c7990f6a9d3c8228248e144115b205a19f02
begunovav2 <av.begunov@tensor.ru>
2018-02-03 16:06:33 +0300

    добавил обработку ошибок в add-component-info

--- gulp/plugins/add-component-info.js
+++ gulp/plugins/add-component-info.js
@@ -1,12 +1,22 @@
 'use strict';
 
 const through = require('through2'),
-   parseJsComponent = require('../../lib/parse-js-component');
+   parseJsComponent = require('../../lib/parse-js-component'),
+   logger = require('../../lib/logger').logger();
 
 module.exports = function() {
    return through.obj(function(file, encoding, callback) {
-      if(file.extname === '.js'){
-         file.componentInfo = parseJsComponent(file.contents.toString());
+      if (file.extname === '.js') {
+         try {
+            file.componentInfo = parseJsComponent(file.contents.toString());
+         } catch (error) {
+            logger.error({
+               message: 'Ошибка при парсинге JS файла',
+               filePath: file.history[0],
+               error: error,
+               moduleInfo: file.moduleInfo
+            });
+         }
       }
       this.push(file);
       callback();
