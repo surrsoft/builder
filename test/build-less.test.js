446c05a01c9c142d00eb1a3578291b096b156f54
begunovav2 <av.begunov@tensor.ru>
2018-02-06 12:45:55 +0300

    добавил тестов для ошибок при компиляции less

--- test/build-less.test.js
+++ test/build-less.test.js
@@ -4,9 +4,11 @@
 require('../lib/logger').setGulpLogger(require('gulplog'));
 
 const chai = require('chai'),
+   chaiAsPromised = require('chai-as-promised'),
    path = require('path'),
    buildLess = require('../lib/build-less');
 
+chai.use(chaiAsPromised);
 chai.should();
 
 const testDirname = path.join(__dirname, 'fixture/build-less');
@@ -89,6 +91,31 @@ describe('build less', function() {
             '  test-mixin: \'mixin there\';\n' +
             `  test-var: \'it is ${themes[i]}\';\n}\n`);
       }
+   });
+   it('less with error', async() => {
+      //строка, где произошла ошибка определяется не корректно из-за добавляемых импортов
+      const filePath = path.join(testDirname, 'AnyModule/bla/bla/long/path/test.less');
+      const text = '@import "notExist";';
+      return buildLess(filePath, text, testDirname).should.be.rejectedWith(
+         `Ошибка компиляции ${testDirname}/AnyModule/bla/bla/long/path/test.less на строке 4: ` +
+         `'notExist.less' wasn't found. Tried - ${testDirname}/AnyModule/bla/bla/long/path/notExist.less,notExist.less`);
+
+   });
+   it('less with error from SBIS3.CONTROLS', async() => {
+      const filePath = path.join(testDirname, 'AnyModule/bla/bla/long/path/test.less');
+      const text = '@import "notExist";';
+      return buildLess(filePath, text, testDirname).should.be.rejectedWith(
+         `Ошибка компиляции ${testDirname}/AnyModule/bla/bla/long/path/test.less на строке 4: ` +
+         `'notExist.less' wasn't found. Tried - ${testDirname}/AnyModule/bla/bla/long/path/notExist.less,notExist.less`);
+
+   });
+   it('less with internal error', async() => {
+      const filePath = path.join(testDirname, 'AnyModule/test.less');
+      const text = '@import "Error";';
+      return buildLess(filePath, text, testDirname).should.be.rejectedWith(
+         `Ошибка компиляции ${testDirname}/AnyModule/Error.less на строке 1: ` +
+         `'notExist.less' wasn't found. Tried - ${testDirname}/AnyModule/notExist.less,notExist.less`);
+
    });
 
 });
