c1d112d0761363f7c4a97ffb4478cff779a03984
begunovav2 <av.begunov@tensor.ru>
2018-02-09 19:43:15 +0300

    сбор фраз для локализации xhtml с тестами

--- test/collect-words.test.js
+++ test/collect-words.test.js
@@ -27,6 +27,18 @@ describe('transliterate', function() {
       words.length.should.equal(0);
    });
 
+   it('empty xhtml', function() {
+      const text = '';
+      const words = collectWords('module', 'file.xhtml', text, []);
+      words.length.should.equal(0);
+   });
+
+   it('empty tmpl', function() {
+      const text = '';
+      const words = collectWords('module', 'file.tmpl', text, []);
+      words.length.should.equal(0);
+   });
+
    it('collect words in js', function() {
       //TODO: Добавить plural
       const moduleDir = 'long/path/moduleName';
@@ -39,6 +51,42 @@ describe('transliterate', function() {
          '/* var testVar3 = rk(\'Test3\', \'TestContext3\'); */\n';
       const words = collectWords(moduleDir, filePath, text, []);
       words.length.should.equal(4); //TODO: очевидно, тут ошибка. должно быть 2
+
+      words[0].ui.should.equal(moduleDir);
+      words[0].module.should.equal(filePath);
+      words[0].key.should.equal('Test1');
+      words[0].context.should.equal('');
+
+      words[1].ui.should.equal(moduleDir);
+      words[1].module.should.equal(filePath);
+      words[1].key.should.equal('Test2');
+      words[1].context.should.equal('TestContext2');
+   });
+
+   it('collect words in xhtml. simple div and span', function() {
+      const moduleDir = 'long/path/moduleName';
+      const filePath = path.join(moduleDir, 'file.xhtml');
+      const text = '<div>Test1</div><span>TestContext2@@Test2</span><!--<div>Текст3</div><span>Текст4</span>-->';
+      const words = collectWords(moduleDir, filePath, text, []);
+      words.length.should.equal(2);
+
+      words[0].ui.should.equal(moduleDir);
+      words[0].module.should.equal(filePath);
+      words[0].key.should.equal('Test1');
+      words[0].context.should.equal('');
+
+      words[1].ui.should.equal(moduleDir);
+      words[1].module.should.equal(filePath);
+      words[1].key.should.equal('Test2');
+      words[1].context.should.equal('TestContext2');
+   });
+
+   it('collect words in tmpl. simple div and span', function() {
+      const moduleDir = 'long/path/moduleName';
+      const filePath = path.join(moduleDir, 'file.tmpl');
+      const text = '<div>Test1</div><span>TestContext2@@Test2</span><!--<div>Текст3</div><span>Текст4</span>-->';
+      const words = collectWords(moduleDir, filePath, text, []);
+      words.length.should.equal(2);
 
       words[0].ui.should.equal(moduleDir);
       words[0].module.should.equal(filePath);
