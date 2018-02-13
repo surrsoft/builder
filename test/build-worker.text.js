b99fb8b69197be09ef16fb85d15d2f6774eb87b7
begunovav2 <av.begunov@tensor.ru>
2018-02-13 17:43:33 +0300

    тест обработки пустых файлов

--- test/build-worker.text.js
+++ test/build-worker.text.js
@@ -10,15 +10,31 @@ chai.should();
 const expect = chai.expect;
 
 describe('build worker', function() {
-   it('test for normal exit', async function() {
+   it('тест для пустых файлов', async function() {
       const pool = workerPool.pool(path.join(__dirname, '../gulp/workers/build-worker.js'));
 
-      const resultparseJsComponent = await pool.exec('parseJsComponent', ['']);
+      try {
+         const resultParseJsComponent = await pool.exec('parseJsComponent', ['']);
+         Object.keys(resultParseJsComponent).length.should.equal(0);
 
-      return pool.terminate();
+         const resultParseRoutes = await pool.exec('parseRoutes', ['']);
+         Object.keys(resultParseRoutes).length.should.equal(0);
+
+         const resourcePath = path.join(__dirname, 'fixture/build-worker/resources');
+         const lessPath = path.join(resourcePath, 'AnyModule/Empty.less');
+         const resultsBuildLess = await pool.exec('buildLess', [[lessPath], resourcePath]);
+         resultsBuildLess.length.should.equal(1);
+         resultsBuildLess[0].hasOwnProperty('error').should.equal(false);
+         resultsBuildLess[0].hasOwnProperty('path').should.equal(true);
+         resultsBuildLess[0].hasOwnProperty('imports').should.equal(true);
+         resultsBuildLess[0].path.should.equal(lessPath);
+         resultsBuildLess[0].imports.length.should.equal(2);
+
+      } finally {
+         pool.terminate();
+      }
    });
-   it('test for error exit', async function() {
-      const promises = [];
+   it('проверяем, что ошибки возвращаются корректно', async function() {
       const pool = workerPool.pool(path.join(__dirname, '../gulp/workers/build-worker.js'));
 
       try {
@@ -26,14 +42,13 @@ describe('build worker', function() {
          await expect(promiseParseJsComponent).to.be.rejected.then(function(error) {
             expect(error).to.have.property('message', 'Ошибка при парсинге: Error: Line 1: Unexpected end of input');
          });
-         promises.push(promiseParseJsComponent);
 
          const promiseParseRoutes = pool.exec('parseRoutes', ['define(']);
          await expect(promiseParseRoutes).to.be.rejected.then(function(error) {
             expect(error).to.have.property('message', 'Ошибка при парсинге: Error: Line 1: Unexpected end of input');
          });
 
-         const resourcePath = path.join(__dirname, 'fixture/build-less/resources');
+         const resourcePath = path.join(__dirname, 'fixture/build-worker/resources');
          const lessPath = path.join(resourcePath, 'AnyModule/Error.less');
          const resultsBuildLess = await pool.exec('buildLess', [[lessPath], resourcePath]);
          resultsBuildLess.length.should.equal(1);
