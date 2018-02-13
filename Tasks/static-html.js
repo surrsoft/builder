549d4be2a142d6a5a4ecf862037c48ff142df668
is.kudryavcev <is.kudryavcev@corp.tensor.ru>
2018-02-13 11:59:58 +0300

    https://online.sbis.ru/opendoc.html?guid=6a92d7b7-1796-4d1e-b68c-f6cdc43a12eb

--- Tasks/static-html.js
+++ Tasks/static-html.js
@@ -87,9 +87,14 @@ module.exports = function(grunt) {
          patterns = this.data.src,
          oldHtml = grunt.file.expand({cwd: applicationRoot}, this.data.html),
          modulesOption = (grunt.option('modules') || '').replace('"', ''),
-         splittedCore = (grunt.option('splitted-core') === undefined) ? false : grunt.option('splitted-core'),
-         multiService = (grunt.option('multi-service') === undefined) ? false : grunt.option('multi-service'),
-         replacePath = (splittedCore && multiService) ? false : true;
+         splittedCore = !!(grunt.option('splitted-core')),
+         multiService = !!(grunt.option('multi-service')),
+         /*
+         Даннный флаг определяет надо ли заменить в статических страничка конструкции типа %{FOO_PATH}, на абсалютные пути.
+          false - если у нас разделённое ядро и несколько сервисов.
+          true - если у нас монолитное ядро или один сервис.
+          */
+         replacePath = !(splittedCore && multiService);
 
       const pathsModules = grunt.file.readJSON(modulesOption);
       if (!Array.isArray(pathsModules)) {
