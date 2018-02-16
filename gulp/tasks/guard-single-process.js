9adaeb25b4b4aebacb71dd6de78c30af4a1c960d
begunovav2 <av.begunov@tensor.ru>
2018-02-16 16:19:50 +0300

    форматирование кода

--- gulp/tasks/guard-single-process.js
+++ gulp/tasks/guard-single-process.js
@@ -12,53 +12,49 @@ let lockFle;
 module.exports = {
    'lock': function lock(config) {
       return function lockGuard() {
-         return new Promise(
-            async(resolve, reject) => {
-               const dir = config.cachePath;
-               await fs.ensureDir(dir);
-               lockFle = path.join(dir, 'builder.lockfile');
+         return new Promise(async(resolve, reject) => {
+            const dir = config.cachePath;
+            await fs.ensureDir(dir);
+            lockFle = path.join(dir, 'builder.lockfile');
 
-               const isFileExist = await fs.pathExists(lockFle);
-               if (isFileExist) {
-                  const errorMessage = 'Похоже, что запущен другой процесс builder в этой же папке, попробуйте перезапустить его позже. ' +
-                     `Если вы уверены, что предыдущий запуск завершился, то удалите файл '${lockFle}' и перезапустите процесс.`;
+            const isFileExist = await fs.pathExists(lockFle);
+            if (isFileExist) {
+               const errorMessage = 'Похоже, что запущен другой процесс builder в этой же папке, попробуйте перезапустить его позже. ' +
+                  `Если вы уверены, что предыдущий запуск завершился, то удалите папку '${dir}' и перезапустите процесс.`;
 
-                  logger.error(errorMessage);
-                  reject(new PluginError({
-                     plugin: 'Guard single process',
-                     message: errorMessage
-                  }));
+               logger.error(errorMessage);
+               reject(new PluginError({
+                  plugin: 'Guard single process',
+                  message: errorMessage
+               }));
 
-               }
-               await fs.ensureFile(lockFle);
-               logger.debug(`Создали файл '${lockFle}'`);
-               resolve();
             }
-         );
+            await fs.ensureFile(lockFle);
+            logger.debug(`Создали файл '${lockFle}'`);
+            resolve();
+         });
       };
    },
    'unlock': function unlock() {
       return function unlockGuard() {
-         return new Promise(
-            async(resolve, reject) => {
-               const isFileExist = await fs.pathExists(lockFle);
-               if (!isFileExist) {
-                  const errorMessage = `В процессе выполнения кто-то удалил файл '${lockFle}'. ` +
-                     'Нет гарантий, что результат не пострадал. Перезапустите процесс.';
+         return new Promise(async(resolve, reject) => {
+            const isFileExist = await fs.pathExists(lockFle);
+            if (!isFileExist) {
+               const errorMessage = `В процессе выполнения кто-то удалил файл '${lockFle}'. ` +
+                  'Нет гарантий, что результат не пострадал. Перезапустите процесс.';
 
-                  logger.error(errorMessage);
-                  reject(new PluginError({
-                     plugin: 'Guard single process',
-                     message: errorMessage
-                  }));
+               logger.error(errorMessage);
+               reject(new PluginError({
+                  plugin: 'Guard single process',
+                  message: errorMessage
+               }));
 
-               } else {
-                  await fs.remove(lockFle);
-                  logger.debug(`Удалили файл '${lockFle}'`);
-                  resolve();
-               }
+            } else {
+               await fs.remove(lockFle);
+               logger.debug(`Удалили файл '${lockFle}'`);
+               resolve();
             }
-         );
+         });
       };
    }
 };
