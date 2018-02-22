b45c9c368cc98acfd0644cf53791319616dafe50
begunovav2 <av.begunov@tensor.ru>
2018-02-22 17:24:05 +0300

    добавил проверок при чтении файла конфигурации

--- gulp/classes/build-configuration.js
+++ gulp/classes/build-configuration.js
@@ -25,31 +25,61 @@ class BuildConfiguration {
          }
       });
 
-      if (!fs.pathExistsSync(configFile)) {
-         throw new Error('Файл конфигурации не задан или файл не существует.');
+      if (!configFile) {
+         throw new Error('Файл конфигурации не задан.');
       }
 
+      if (!fs.pathExistsSync(configFile)) {
+         throw new Error(`Файл конфигурации '${configFile}' не существует.`);
+      }
+
+      const startErrorMessage = `Файл конфигурации ${configFile} не корректен.`;
       try {
          this.rawConfig = fs.readJSONSync(configFile);
       } catch (e) {
-         throw new Error('Файл конфигурации не корректен. Он должен представлять собой JSON-документ в кодировке UTF8. Ошибка: ' + e.message);
+         e.message = `${startErrorMessage} Он должен представлять собой JSON-документ в кодировке UTF8. Ошибка: ${e.message}`;
+         throw e;
       }
 
-      if (this.rawConfig.hasOwnProperty('mode')) {
-         const mode = this.rawConfig.mode;
-         if (mode !== 'release' && mode !== 'debug') {
-            throw new Error('Параметр mode может принимать значения "release" и "debug"');
-         }
-         this.isReleaseMode = mode === 'release';
-      } else {
-         throw new Error('Не задан обязательный параметр mode');
+      if (!this.rawConfig.hasOwnProperty('mode')) {
+         throw new Error(`${startErrorMessage} Не задан обязательный параметр mode`);
       }
+      const mode = this.rawConfig.mode;
+      if (mode !== 'release' && mode !== 'debug') {
+         throw new Error(`${startErrorMessage} Параметр mode может принимать значения "release" и "debug"`);
+      }
+      this.isReleaseMode = mode === 'release';
+
 
       this.outputPath = this.rawConfig.output;
+      if (!this.outputPath) {
+         throw new Error(`${startErrorMessage} Не задан обязательный параметр output`);
+      }
+
       this.cachePath = this.rawConfig.cache;
+      if (!this.cachePath) {
+         throw new Error(`${startErrorMessage} Не задан обязательный параметр cache`);
+      }
+
       this.localizations = this.rawConfig.localizations;
       this.defaultLocalization = this.rawConfig['default-localization'];
+
+      if (!this.rawConfig.hasOwnProperty('modules')) {
+         throw new Error(`${startErrorMessage} Не задан обязательный параметр modules`);
+      }
+      if (!Array.isArray(this.rawConfig.modules)) {
+         throw new Error(`${startErrorMessage} Параметр modules должен быть массивом`);
+      }
+      if (this.rawConfig.modules.length === 0) {
+         throw new Error(`${startErrorMessage} Массив modules должен быть не пустым`);
+      }
       for (const module of this.rawConfig.modules) {
+         if (!module.hasOwnProperty('path') || !module.path) {
+            throw new Error(`${startErrorMessage} Для модуля не задан обязательный параметр path`);
+         }
+         if (!fs.pathExistsSync(module.path)) {
+            throw new Error(`${startErrorMessage} Директория ${module.path} не существует`);
+         }
          this.modules.push(new ModuleInfo(
             module.name,
             module.responsible,
