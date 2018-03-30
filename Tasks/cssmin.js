0715915faab55911ab76170af36b21ac2f30abf7
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2018-03-31 00:18:06 +0300

    Поправил таску cssmin, чтобы каждая css-ка минифицировалась отдельно и не обьединялась в группы с другими cssками подобного имени. Также поправил добавление .min суффикса в module-dependencies https://online.sbis.ru/opendoc.html?guid=e8d9cc3b-3b7f-4494-bd81-7388a9426b6f

--- Tasks/cssmin.js
+++ Tasks/cssmin.js
@@ -70,7 +70,7 @@ module.exports = function(grunt) {
          logger.error({
             message: 'CSS minification failed',
             error: err,
-            filePath: availableFiles.toString()
+            filePath: fileSourcePath
          });
       }
 
