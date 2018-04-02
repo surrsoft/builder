23c80302ef467a975a9bf2c5c27ab9d2146cf9f6
begunovav2 <av.begunov@tensor.ru>
2018-04-02 14:43:21 +0300

    поправил компиляцию less в grunt задаче

--- Tasks/less1by1.js
+++ Tasks/less1by1.js
@@ -1,70 +1,97 @@
 'use strict';
 
 const helpers = require('../lib/helpers'),
+   transliterate = require('../lib/transliterate'),
    logger = require('../lib/logger').logger(),
    buildLess = require('../lib/build-less'),
    fs = require('fs-extra'),
    path = require('path'),
-   applicationRoot = path.join(process.env.ROOT, process.env.APPROOT);
+   pMap = require('p-map');
 
 module.exports = function less1by1Task(grunt) {
    const root = grunt.option('root') || '',
       app = grunt.option('application') || '',
-      rootPath = path.join(root, app);
+      modulesOption = (grunt.option('modules') || '').replace('"', ''),
+      rootPath = path.join(root, app),
+      resourcePath = path.join(rootPath, 'resources');
 
-   grunt.registerMultiTask('less1by1', 'Компилит каждую лесску, ложит cssку рядом. Умеет в темы', function() {
+   grunt.registerMultiTask('less1by1', 'Задача компиляци less в css. Поддерживает темы', async function() {
       logger.debug('Запускается задача less1by1.');
 
       const taskDone = this.async(); //eslint-disable-line no-invalid-this
 
-      helpers.recurse(rootPath, async function(filePath, cb) {
-         try {
-            const relativePath = path.relative(rootPath, filePath);
-            if (!helpers.validateFile(relativePath, ['ws/**/*.less', 'resources/**/*.less'])) {
-               cb();
-               return;
+      try {
+         if (!modulesOption) {
+            logger.error('Parameter "modules" not found');
+            return;
+         }
+         const modulesPaths = grunt.file.readJSON(modulesOption);
+         if (!Array.isArray(modulesPaths)) {
+            logger.error('Parameter "modules" incorrect');
+            return;
+         }
+
+         let sbis3ControlsPath = '';
+         for (const modulePath of modulesPaths) {
+            if (path.basename(modulePath) === 'SBIS3.CONTROLS') {
+               sbis3ControlsPath = modulePath;
             }
+         }
 
-            const newFullPath = filePath.replace('.less', '.css');
-            if (await fs.pathExists(newFullPath)) {
-               //если файл уже есть, то не нужно его перезаписывать.
-               //иначе при деплое локального стенда мы перезапишем css в исходниках.
-               //просто ругаемся и ждём, что поправят.
-               const message = `Существующий CSS-файл мешает записи результата компиляции '${filePath}'. ` +
-                  'Необходимо удалить лишний CSS-файл';
+         await pMap(modulesPaths, async(modulePath) => {
+            const files = [];
+            const moduleName = path.basename(modulePath);
+            await helpers.recursiveReadDir(modulePath, files, '.less');
+            await pMap(files, async(lessFilePath) => {
+               try {
+                  const cssFilePath = lessFilePath.replace('.less', '.css');
+                  if (await fs.pathExists(cssFilePath)) {
+                     //если файл уже есть, то не нужно его перезаписывать.
+                     //иначе при деплое локального стенда мы перезапишем css в исходниках.
+                     //просто ругаемся и ждём, что поправят.
+                     const message = `Существующий CSS-файл мешает записи результата компиляции '${lessFilePath}'. ` +
+                        'Необходимо удалить лишний CSS-файл';
 
-               //монолитный ws уже должен быть со скомпилированными less, поэтому выводим не предупреждение, а просто информацию
-               if (helpers.prettifyPath(newFullPath).includes('/ws/')) {
-                  logger.info(message);
-               } else {
+                     logger.warning({
+                        message: message,
+                        filePath: cssFilePath
+                     });
+                     return;
+                  }
+
+                  const data = await fs.readFile(lessFilePath);
+                  const result = await buildLess(lessFilePath, data.toString(), modulePath, sbis3ControlsPath);
+                  if (result.ignoreMessage) {
+                     logger.debug(result.ignoreMessage);
+                  } else {
+                     const relativeLessFilePath = path.relative(modulePath, lessFilePath);
+                     const relativeResultCssFilePath = transliterate(path.join(moduleName, relativeLessFilePath.replace('.less', '.css')));
+                     const resultCssPath = path.join(resourcePath, relativeResultCssFilePath);
+                     await fs.writeFile(resultCssPath, result.text, {flag: 'w'});
+                     logger.debug(`file ${resultCssPath} successfully compiled`);
+                  }
+
+               } catch (error) {
                   logger.warning({
-                     message: message,
-                     filePath: newFullPath
+                     message: 'Ошибка при компиляции less файла',
+                     error: error,
+                     filePath: lessFilePath
                   });
                }
-            }
-
-            const data = await fs.readFile(filePath);
-            const resourcePath = path.join(applicationRoot, 'resources');
-            const result = await buildLess(filePath, data.toString(), resourcePath);
-
-            if (result.ignoreMessage) {
-               logger.debug(result.ignoreMessage);
-            } else {
-               await fs.writeFile(newFullPath, result.text, {flag: 'w'});
-               logger.debug(`file ${newFullPath} successfully compiled`);
-            }
-         } catch (error) {
-            logger.warning({
-               message: 'Ошибка при компиляции less файла',
-               error: error,
-               filePath: filePath
+            }, {
+               concurrency: 10
             });
-         }
-         cb();
-      }, function() {
+         }, {
+            concurrency: 2
+         });
+
          logger.debug('Задача less1by1 выполнена.');
-         taskDone();
-      });
+      } catch (error) {
+         logger.error({
+            message: 'Ошибка при выполнении задачи less1by1',
+            error: error
+         });
+      }
+      taskDone();
    });
 };
