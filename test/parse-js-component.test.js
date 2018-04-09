b3bb5fc448d62104cc67248275773867c9c18f10
is.kudryavcev <is.kudryavcev@corp.tensor.ru>
2018-04-09 15:45:56 +0300

    https://online.sbis.ru/opendoc.html?guid=027b1ef1-5600-44bc-880f-178347da0cd6

--- test/parse-js-component.test.js
+++ test/parse-js-component.test.js
@@ -82,13 +82,14 @@ describe('parse js component', function() {
    it('declare dependences module', () => {
       const result = parseJsComponent('define("My.Module/Name", ["My.Dep/Name1", "My.Dep/Name2"], function(){});');
       Object.getOwnPropertyNames(result).length.should.equal(2);
-      result.componentDep.should.have.members(["My.Dep/Name1", "My.Dep/Name2"]);
+      result.componentDep.should.have.members(['My.Dep/Name1', 'My.Dep/Name2']);
    });
    it('declare dependences module, empty name', () => {
       const result = parseJsComponent('define(["My.Dep/Name1", "My.Dep/Name2"], function(){});');
+
       //
       Object.getOwnPropertyNames(result).length.should.equal(1);
-      result.componentDep.should.have.members(["My.Dep/Name1", "My.Dep/Name2"]);
+      result.componentDep.should.have.members(['My.Dep/Name1', 'My.Dep/Name2']);
    });
 });
 
