c47139d1620bb0297f3a7d90afeeb3e99c7522da
begunovav2 <av.begunov@tensor.ru>
2018-05-14 14:46:50 +0300

    рефакторинг таски normalize-key https://online.sbis.ru/opendoc.html?guid=5f4dbe18-5ba3-4fa8-80ba-3adea0e2e6ff

---
+++ test/normalize-key.test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+require('./init-test');
+
+const path = require('path'),
+   fs = require('fs-extra'),
+   normalizeKey = require('../lib/i18n/normalize-key');
+
+const workspaceFolder = path.join(__dirname, 'workspace'),
+   fixtureFolder = path.join(__dirname, 'fixture/normalize-key');
+
+const languages = ['en-US', 'ru=RU'];
+
+describe('lib/i18n/normalize-key.js', function() {
+   before(async function() {
+      await fs.remove(workspaceFolder);
+      await fs.copy(fixtureFolder, workspaceFolder);
+   });
+   after(async function() {
+      await fs.remove(workspaceFolder);
+   });
+   it('main', async() => {
+      await normalizeKey(workspaceFolder, languages);
+      const dictModule1 = await fs.readJSON(path.join(workspaceFolder, 'Module1/lang/en-US/en-US.json'));
+      dictModule1.should.deep.equal({
+         'test': 'Значение, которое должно перезатереть всех',
+         'simple1': 'simple1'
+      });
+
+      const dictModule2 = await fs.readJSON(path.join(workspaceFolder, 'Module2/lang/en-US/en-US.json'));
+      dictModule2.should.deep.equal({
+         'test': 'Значение, которое должно перезатереть всех',
+         'simple2': 'simple2'
+      });
+
+      const dictModule3 = await fs.readJSON(path.join(workspaceFolder, 'Module3/lang/en-US/en-US.json'));
+      dictModule3.should.deep.equal({
+         'test': 'Значение, которое должно перезатереть всех',
+         'simple3': 'simple3'
+      });
+   });
+});
