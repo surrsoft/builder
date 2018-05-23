a3d8ddcaa40ce810a98e039f11642d1685e1a3d3
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2018-05-23 19:40:47 +0300

    Продолжаю дорабатывать кастомную паковку на промисах

--- Tasks/pack/custompack.js
+++ Tasks/pack/custompack.js
@@ -1,9 +1,11 @@
-/* eslint-disable no-unlimited-disable*/
-/* eslint-disable */
-//sorry about that ¯\_(ツ)_/¯
+/* eslint-disable no-invalid-this */
+/* eslint-disable no-restricted-properties */
+'use strict';
 
 const
    customPackage = require('../../lib/pack/customPackage'),
+   commonPackage = require('../../packer/lib/commonPackage'),
+   packerDictionary = require('../../packer/tasks/lib/packDictionary'),
    logger = require('../../lib/logger').logger(),
    modDeps = require('../../packer/lib/moduleDependencies'),
    path = require('path'),
@@ -27,14 +29,13 @@ async function generatePackageJsonConfigs(depsTree, configs, applicationRoot, bu
    await pMap(
       configs,
       async config => {
+         const configNum = config.configNum ? 'конфигурация №' + config.configNum : '';
          try {
-            const configNum = config.configNum ? 'конфигурация №' + config.configNum : '';
+
             /**
              * результатом выполнения функции мы сделаем объект, он будет содержать ряд опций:
              * 1)bundles: в нём будут храниться подвергнутые изменениям бандлы.
              * 2)bundlesRoute: тоже самое что и выше, только для bundlesRoute.
-             * 3)oldBundles: для гальпа.
-             * 4)oldBundlesRoute: для гальпа.
              */
             const currentResult = await generateCustomPackage(depsTree, config, applicationRoot, bundlesOptions.splittedCore);
             Object.keys(currentResult).forEach(key => results[key].push(currentResult[key]));
@@ -51,58 +52,40 @@ async function generatePackageJsonConfigs(depsTree, configs, applicationRoot, bu
       }
    );
 
-   await saveCustomPackResults(results, applicationRoot, bundlesOptions.splittedCore)
-};
+   await saveCustomPackResults(results, applicationRoot, bundlesOptions.splittedCore);
+}
 
-async function writeCustomPackage() {
+async function writeCustomPackage(packageConfig, applicationRoot) {
+   const
+      currentFileExists = await fs.pathExists(packageConfig.outputFile),
+      originalFileExists = await fs.pathExists(customPackage.originalPath(packageConfig.outputFile));
+
    // Не будем портить оригинальный файл.
-   if (await fs.pathExists(packageConfig.outputFile) && !(await fs.pathExists(originalPath(packageConfig.outputFile)))) {
-      await commonPackage.copyFile(cfg.outputFile, originalPath(cfg.outputFile), callBack);
-   } else {
-      callBack();
+   if (currentFileExists && !originalFileExists) {
+      await fs.copy(packageConfig.outputFile, customPackage.originalPath(packageConfig.outputFile));
    }
-
-   function callBack(err) {
-      if (err) {
-         done(err);
-      } else {
-         commonPackage.limitingNativePackFiles(cfg.orderQueue, 10, root, function(err, result) {
-            if (err) {
-               //удаляем из бандлов конфигурацию, если пакет по итогу не смог собраться
-               delete bundlesOptions.bundles[cfg.packagePath];
-               done(err);
-            } else {
-               grunt.file.write(cfg.outputFile, result ? result.reduce(function concat(res, modContent) {
-                  return res + (res ? '\n' : '') + modContent;
-               }, '') : '');
-               done(null);
-            }
-         });
-      }
-   }
+   const result = await commonPackage.limitingNativePackFiles(packageConfig.orderQueue, applicationRoot);
+   await fs.writeFile(packageConfig.outputFile, result ? result.reduce((res, modContent) => res + (res ? '\n' : '') + modContent) : '');
 }
 
 async function generateCustomPackage(depsTree, applicationRoot, packageConfig, isSplittedCore) {
-   const result = {
-      bundles: {},
-      bundlesRoute: {},
-      oldBundles: {},
-      oldBundlesRoute: {}
-   };
+   const
+      outputFile = customPackage.getOutputFile(packageConfig, applicationRoot, depsTree, isSplittedCore),
+      packagePath = customPackage.getBundlePath(outputFile, applicationRoot, isSplittedCore ? 'resources/WS.Core' : 'ws'),
+      result = {
+         bundles: {},
+         bundlesRoute: {}
+      };
 
    let
-      orderQueue,
-      outputFile,
-      packagePath;
+      orderQueue;
 
    if (packageConfig.isBadConfig) {
       throw new Error('Конфиг для кастомного пакета должен содержать опцию include для нового вида паковки.');
    }
    orderQueue = customPackage.getOrderQueue(depsTree, packageConfig, applicationRoot);
-   outputFile = customPackage.getOutputFile(packageConfig, applicationRoot, depsTree, isSplittedCore);
-   packagePath = customPackage.getBundlePath(outputFile, applicationRoot, wsRoot);
 
-   orderQueue = orderQueue.filter(function (node) {
+   orderQueue = orderQueue.filter(function(node) {
       if (node.plugin === 'js') {
          return node.amd;
       }
@@ -120,7 +103,7 @@ async function generateCustomPackage(depsTree, applicationRoot, packageConfig, i
    packageConfig.packagePath = packagePath;
    result.output = packageConfig.outputFile;
 
-   await writeCustomPackage(packageConfig);
+   await writeCustomPackage(packageConfig, applicationRoot);
    return result;
 }
 
@@ -131,23 +114,30 @@ async function generateCustomPackage(depsTree, applicationRoot, packageConfig, i
  * @returns {Promise<void>}
  */
 async function saveBundlesForEachModule(applicationRoot, result) {
-   Object.keys(result.bundles).forEach(async currentBundle => {
-      let
-         bundlePath = path.normalize(path.join(applicationRoot, `${currentBundle.match(/^resources\/[^/]+/)}`, 'bundlesRoute.json')),
-         currentModules = result.bundles[currentBundle],
-         bundleRouteToWrite = {};
+   await pMap(
+      result.bundles,
+      async currentBundle => {
+         const
+            bundlePath = path.normalize(path.join(applicationRoot, `${currentBundle.match(/^resources\/[^/]+/)}`, 'bundlesRoute.json')),
+            currentModules = result.bundles[currentBundle];
 
-      if (await fs.pathExists(bundlePath)) {
-         bundleRouteToWrite = await fs.readJson(bundlePath)
-      }
+         let bundleRouteToWrite = {};
 
-      currentModules.forEach(function(node) {
-         if (node.indexOf('css!') === -1) {
-            bundleRouteToWrite[node] = currentBundle;
+         if (await fs.pathExists(bundlePath)) {
+            bundleRouteToWrite = await fs.readJson(bundlePath);
          }
-      });
-      await fs.writeJson(bundlePath, bundleRouteToWrite);
-   });
+
+         currentModules.forEach(function(node) {
+            if (node.indexOf('css!') === -1) {
+               bundleRouteToWrite[node] = currentBundle;
+            }
+         });
+         await fs.writeJson(bundlePath, bundleRouteToWrite);
+      },
+      {
+         concurrency: 10
+      }
+   );
 }
 
 /**
@@ -158,7 +148,7 @@ async function saveCustomPackResults(result, applicationRoot, splittedCore) {
    const
       bundlesPath = 'ext/requirejs',
       wsRoot = splittedCore ? 'resources/WS.Core' : 'ws',
-      bundlesRoutePath = path.join(wsRoot, bundlesPath, 'bundlesRoute').replace(dblSlashes, '/'),
+      bundlesRoutePath = path.join(wsRoot, bundlesPath, 'bundlesRoute').replace(/\\/g, '/'),
       pathsToSave = {
          bundles: path.join(applicationRoot, wsRoot, bundlesPath, 'bundles.js'),
          output: path.join(applicationRoot, wsRoot, bundlesPath, 'output.json'),
@@ -173,26 +163,28 @@ async function saveCustomPackResults(result, applicationRoot, splittedCore) {
       Object.keys(pathsToSave),
       async key => {
          let json;
+
          //нам надо проверить, что нужные файлы уже были сгенерены(инкрементальная сборка)
          if (await fs.pathExists(pathsToSave[key])) {
-            switch(key) {
+            switch (key) {
                case 'bundlesRouteJson':
                case 'output':
                   json = await fs.readJson(pathsToSave[key]);
                   break;
-               case 'bundles':
+               default://bundles
                   //для bundles надо сначала удалить лишний код, а только потом парсить json
-                  json = JSON.parse((await fs.readFile(pathsToSave[key], 'utf8')).slice(8, -1))
+                  json = JSON.parse((await fs.readFile(pathsToSave[key], 'utf8')).slice(8, -1));
                   break;
             }
          }
+
          //если файл существует и мы его прочитали, то просто дополняем его свежесгенеренными результатами.
          if (json) {
             Object.keys(result[key]).forEach(option => {
                json[option] = result[key][option];
             });
          }
-         switch(key) {
+         switch (key) {
             case 'bundlesRouteJson':
                await fs.outputJson(pathsToSave[key], json);
                logger.debug(`Записали bundlesRoute.json по пути: ${pathsToSave[key]}`);
@@ -201,9 +193,10 @@ async function saveCustomPackResults(result, applicationRoot, splittedCore) {
                await fs.outputJson(pathsToSave[key], json);
                logger.debug(`Записали output.json по пути: ${pathsToSave[key]}`);
                break;
-            case 'bundles':
+            default://bundles
                await fs.writeFile(pathsToSave[key], `bundles=${JSON.stringify(json)};`);
                logger.debug(`Записали bundles.js по пути: ${pathsToSave[key]}`);
+
                /**
                 * Таска минификации выполняется до кастомной паковки, поэтому мы должны для СП также
                 * сохранить .min бандл
@@ -219,15 +212,15 @@ async function saveCustomPackResults(result, applicationRoot, splittedCore) {
 }
 
 module.exports = function gruntCustomPack(grunt) {
-   const gruntGetAllConfigs = async (applicationRoot, files) => {
-      let promises = [];
+   const gruntGetAllConfigs = async(applicationRoot, files) => {
+      const promises = [];
       files.forEach((file) => {
          promises.push(customPackage.getConfigsFromPackageJson(path.join(applicationRoot, file)));
       });
       return (await Promise.all(promises)).reduce((previousValue, current) => previousValue.concat(current));
    };
 
-   grunt.registerMultiTask('custompack', 'Задача кастомной паковки', async function () {
+   grunt.registerMultiTask('custompack', 'Задача кастомной паковки', async function() {
       logger.info('Запускается задача создания кастомных пакетов.');
       try {
          const
@@ -239,12 +232,9 @@ module.exports = function gruntCustomPack(grunt) {
             },
             applicationRoot = path.join(self.data.root, self.data.application),
             done = self.async(),
-            dblSlashes = /\\/g,
-            bundlesPath = 'ext/requirejs',
 
             //wsRoot важен для обоих видов сборок, оставляем как в гранте, так и гальпе, выносить в отдельную функцию не вижу смысла
             wsRoot = await fs.pathExists(path.join(applicationRoot, 'resources/WS.Core')) ? 'resources/WS.Core' : 'ws',
-            //
             depsTree = await modDeps.getDependencyGraph(applicationRoot);
 
          let sourceFiles = grunt.file.expand({cwd: applicationRoot}, this.data.src);
@@ -256,7 +246,7 @@ module.exports = function gruntCustomPack(grunt) {
           * Будем чекать рассматриваемый конфиг функцией checkConfigPathForSplittedCore
           */
          if (wsRoot !== 'ws') {
-            sourceFiles = sourceFiles.filter(function (pathToSource) {
+            sourceFiles = sourceFiles.filter(function(pathToSource) {
                return customPackage.checkConfigPathForSplittedCore(pathToSource);
             });
          }
@@ -269,11 +259,6 @@ module.exports = function gruntCustomPack(grunt) {
          const configsArray = await gruntGetAllConfigs(applicationRoot, sourceFiles);
 
          bundlesOptions.splittedCore = this.data.splittedCore;
-         /**
-          * Для конкретного конфига генерим непосредственно пакет.
-          * Общая функция для гальпа и гранта
-          * TODO доработать
-          */
          await generatePackageJsonConfigs(depsTree, configsArray, applicationRoot, bundlesOptions);
          logger.info('Задача создания кастомных пакетов завершена.');
          done();
@@ -284,4 +269,4 @@ module.exports = function gruntCustomPack(grunt) {
          });
       }
    });
-}
+};
