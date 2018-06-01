8cbf561c1721dbe15428fbf1f748fc5dfbdd7f9c
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2018-06-01 18:54:41 +0300

    Добавляем версионирование в статическую паковку css и js https://online.sbis.ru/opendoc.html?guid=e22f7581-2426-41ce-b1ca-66bd9c056eb2

--- packer/tasks/lib/packCSS.js
+++ packer/tasks/lib/packCSS.js
@@ -49,34 +49,35 @@ function cssCollector(dom) {
 }
 
 function cssPacker(filesToPack, currentRoot) {
-   return cssHelpers.splitIntoBatches(
-      4000,
-      cssHelpers.bumpImportsUp(
-         Object.keys(filesToPack).map(filePath => cssHelpers.rebaseUrls(currentRoot, filePath, filesToPack[filePath])).join('\n')
-      )
+   return cssHelpers.bumpImportsUp(
+      Object.keys(filesToPack).map(filePath => cssHelpers.rebaseUrls(currentRoot, filePath, filesToPack[filePath])).join('\n')
    );
 }
 
-function cssGetTargetNode(dom, filePath) {
+function cssGetTargetNode(dom, filePath, buildNumber) {
+   let linkPath = filePath;
+   if (buildNumber) {
+      linkPath = linkPath.replace(/\.css$/, `.v${buildNumber}.css`);
+   }
    return domHelpers.mkDomNode(dom, 'link', {
       rel: 'stylesheet',
-      href: `/${helpers.prettifyPath(filePath)}`
+      href: `/${helpers.prettifyPath(linkPath)}`
    });
 }
 
 
-function packageSingleCss(filePath, dom, root, packageHome) {
-   return domHelpers.packageSingleFile(filePath, dom, root, packageHome, cssCollector, cssPacker, cssGetTargetNode, 'css');
+function packageSingleCss(filePath, dom, root, packageHome, buildNumber) {
+   return domHelpers.packageSingleFile(filePath, dom, root, packageHome, buildNumber, cssCollector, cssPacker, cssGetTargetNode, 'css');
 }
 
 module.exports = {
    packageSingleCss,
-   gruntPackCSS(htmlFiles, root, packageHome) {
+   gruntPackCSS(htmlFiles, root, packageHome, buildNumber) {
       return pMap(
          htmlFiles,
          async(filePath) => {
             const dom = domHelpers.domify(await fs.readFile(filePath, 'utf-8'));
-            const newDom = await packageSingleCss(filePath, dom, root, packageHome);
+            const newDom = await packageSingleCss(filePath, dom, root, packageHome, buildNumber);
             await fs.writeFile(filePath, domHelpers.stringify(newDom));
          },
          { concurrency: 20 }
