cf456f5d7b1ecdecbdf042be704814920afb2185
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2018-06-04 19:23:19 +0300

    Читаем module-dependencies из кэша гальпа и начинаем покрывать кастомную паковку тестами https://online.sbis.ru/opendoc.html?guid=ce7c8ca8-4c4c-4ef1-a5e3-37f766949596

--- test/fixture/custompack/InterfaceModule1/amdModule.js
+++ test/fixture/custompack/InterfaceModule1/amdModule.js
@@ -1,5 +1,5 @@
-define("InterfaceModule1/amdModule", ["css!"], function() {
+define('InterfaceModule1/amdModule', ['css!'], function() {
    return {
       _moduleName: 'InterfaceModule1/amdModule'
-   }
+   };
 });
