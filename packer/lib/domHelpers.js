4df22c65c3f62f8b27464e6e0c5d328fd517e466
Колбешин Ф.А <fa.kolbeshin@tensor.ru>
2018-06-05 21:46:17 +0300

    Правим место подключение статического css-пакета для таски packcss https://online.sbis.ru/opendoc.html?guid=ecfce283-7070-4d6a-a8b7-f4c355f91933

--- packer/lib/domHelpers.js
+++ packer/lib/domHelpers.js
@@ -155,14 +155,37 @@ async function packageSingleFile(
       // Remove initial collection from DOM
       removeDomCollection(result.nodes);
 
+      let cssTarget, parentTargetNode, openComment, closeComment;
+      if (ext === 'css') {
+         cssTarget = dom.getElementById('ws-include-css');
+
+         // залоггируем статические html, которые не имеют линки с атрибутом ws-include-css
+         if (!cssTarget) {
+            logger.info(`location: ${htmlFilePath}. Для данной html не прописана линка с атрибутом ws-include-css.` +
+               'Стили будут вставлены в конце head, что может привести к изменению порядка подгрузки стилей.');
+            cssTarget = dom.getElementsByTagName('head')[0].lastChild;
+         }
+
+         parentTargetNode = cssTarget.parentNode;
+         openComment = mkCommentNode(cssTarget.ownerDocument, '[packedScripts]');
+         closeComment = mkCommentNode(cssTarget.ownerDocument, '[/packedScripts]');
+         parentTargetNode.insertBefore(openComment, cssTarget);
+      }
+
       // For each packed file create new DOM node and attach it to document
       for (const aSingleFile of packedFiles) {
          const newNode = nodeProducer(dom, path.relative(root, aSingleFile), buildNumber);
-         if (result.before) {
+         if (cssTarget) {
+            parentTargetNode.insertBefore(newNode, cssTarget);
+         } else if (result.before) {
             result.before.parentNode.insertBefore(newNode, result.before);
          } else {
             dom.getElementsByTagName('head')[0].appendChild(newNode);
          }
+      }
+
+      if (ext === 'css') {
+         parentTargetNode.insertBefore(closeComment, cssTarget);
       }
    }
    return dom;
