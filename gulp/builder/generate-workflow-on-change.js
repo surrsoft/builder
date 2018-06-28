8151145330298f49bb103eb79d2507e80e1c082c
begunovav2 <av.begunov@tensor.ru>
2018-06-28 16:51:13 +0300

    если модуль был создан через симлинк, то обновление файла через file watcher не работало https://online.sbis.ru/opendoc.html?guid=24bba05c-76c9-47e0-b5bd-2fbf321c337f

--- gulp/builder/generate-workflow-on-change.js
+++ gulp/builder/generate-workflow-on-change.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-sync */
 /**
  * Генерирует поток выполнения сборки одного less файла при измении
  * Вызывается из WebStorm, например.
@@ -10,6 +11,7 @@ const path = require('path'),
    gulp = require('gulp'),
    gulpIf = require('gulp-if'),
    os = require('os'),
+   fs = require('fs-extra'),
    workerPool = require('workerpool'),
    gulpRename = require('gulp-rename'),
    gulpChmod = require('gulp-chmod'),
@@ -39,7 +41,7 @@ const {
 function generateBuildWorkflowOnChange(processArgv) {
    // загрузка конфигурации должна быть синхронной, иначе не построятся задачи для сборки модулей
    const config = new Configuration();
-   config.loadSync(processArgv); // eslint-disable-line no-sync
+   config.loadSync(processArgv);
 
    const { filePath } = ConfigurationReader.getProcessParameters(processArgv);
    if (!filePath) {
@@ -61,6 +63,7 @@ function generateBuildWorkflowOnChange(processArgv) {
    // guardSingleProcess пришлось убрать из-за того что WebStorm может вызвать несколько процессов параллельно
    return gulp.series(
       generateTaskForLoadChangesStore(changesStore),
+      generateTaskForCheckVersion(changesStore),
       generateTaskForBuildFile(changesStore, config, pool, filePath),
       generateTaskForTerminatePool(pool)
    );
@@ -70,10 +73,23 @@ function generateTaskForBuildFile(changesStore, config, pool, filePath) {
    let currentModuleInfo;
    let sbis3ControlsPath = '';
    const pathsForImportSet = new Set();
+   let filePathInProject = filePath;
    for (const moduleInfo of config.modules) {
-      const relativePath = path.relative(moduleInfo.path, filePath);
-      if (!relativePath.includes('..') && !path.isAbsolute(relativePath)) {
-         currentModuleInfo = moduleInfo;
+      if (!currentModuleInfo) {
+         let relativePath = path.relative(moduleInfo.path, filePath);
+
+         // на windows если два файла на разных дисках, то path.relative даёт путь от диска, без ..
+         if (!relativePath.includes('..') && !path.isAbsolute(relativePath)) {
+            currentModuleInfo = moduleInfo;
+         } else {
+            // если модуль задан через симлинк, попробуем сопоставить файл и модуль
+            const realModulePath = fs.realpathSync(moduleInfo.path);
+            relativePath = path.relative(realModulePath, filePath);
+            if (!relativePath.includes('..') && !path.isAbsolute(relativePath)) {
+               currentModuleInfo = moduleInfo;
+               filePathInProject = path.join(moduleInfo.path, relativePath);
+            }
+         }
       }
       pathsForImportSet.add(path.dirname(moduleInfo.path));
       if (path.basename(moduleInfo.path) === 'SBIS3.CONTROLS') {
@@ -83,14 +99,14 @@ function generateTaskForBuildFile(changesStore, config, pool, filePath) {
 
    const pathsForImport = [...pathsForImportSet];
    if (!currentModuleInfo) {
-      logger.info(`Файл ${filePath} вне проекта`);
+      logger.info(`Файл ${filePathInProject} вне проекта`);
       return function buildModule(done) {
          done();
       };
    }
    return function buildModule() {
       return gulp
-         .src(filePath, { dot: false, nodir: true, base: currentModuleInfo.path })
+         .src(filePathInProject, { dot: false, nodir: true, base: currentModuleInfo.path })
          .pipe(
             plumber({
                errorHandler(err) {
@@ -119,6 +135,18 @@ function generateTaskForBuildFile(changesStore, config, pool, filePath) {
                gulp.dest(currentModuleInfo.output)
             )
          );
+   };
+}
+function generateTaskForCheckVersion(changesStore) {
+   return function checkBuilderVersion(done) {
+      if (changesStore.lastStore.versionOfBuilder !== changesStore.currentStore.versionOfBuilder) {
+         logger.error(
+            `Текущая версия Builder'а (${changesStore.currentStore.versionOfBuilder}) не совпадает с версией, ` +
+            `сохранённой в кеше (${changesStore.lastStore.versionOfBuilder}). ` +
+            'Вероятно, необходимо передеплоить стенд.'
+         );
+      }
+      done();
    };
 }
 module.exports = generateBuildWorkflowOnChange;
