0af11841df5cfe00b30f27664dab9634cf4ec716
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2018-07-03 18:53:03 +0300

    Правим словари локализации для кастомной паковки в соответствии с архитектурой Gulp, оставляя обратную совместимость с Grunt https://online.sbis.ru/opendoc.html?guid=336f00bc-70d4-46ed-bea6-b61c0fa04560

--- packer/lib/commonPackage.js
+++ packer/lib/commonPackage.js
@@ -252,10 +252,11 @@ function prepareResultQueue(orderQueue, applicationRoot, availableLanguage) {
  * @param loader
  * @param module
  * @param base
+ * @param availableLanguage
  * @param themeName
  * @returns {Promise<any>}
  */
-function promisifyLoader(loader, module, base, themeName) {
+function promisifyLoader(loader, module, base, availableLanguage, themeName) {
    return new Promise((resolve, reject) => {
       loader(
          module,
@@ -266,6 +267,7 @@ function promisifyLoader(loader, module, base, themeName) {
             }
             return resolve(result);
          },
+         availableLanguage,
          themeName
       );
    });
@@ -284,7 +286,7 @@ function promisifyLoader(loader, module, base, themeName) {
  * Относительно этой папки будут высчитаны новые пути в ссылках
  * @param {nativePackFiles~callback} done
  */
-async function limitingNativePackFiles(filesToPack, base) {
+async function limitingNativePackFiles(filesToPack, base, availableLanguage, defaultLanguage) {
    if (filesToPack && filesToPack.length) {
       const result = [];
 
@@ -310,7 +312,15 @@ async function limitingNativePackFiles(filesToPack, base) {
             }
 
             try {
-               result.push(await promisifyLoader(getLoader(module.plugin), module, base));
+               result.push(await promisifyLoader(
+                  getLoader(module.plugin),
+                  module,
+                  base,
+                  {
+                     availableLanguage,
+                     defaultLanguage
+                  }
+               ));
             } catch (error) {
                logger.warning({
                   message: 'Ошибка при чтении файла во время кастомной паковки',
