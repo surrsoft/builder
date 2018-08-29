82460811634b859f2bf6b9e03e0f8ea7d6b8fff4
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2018-08-29 15:23:17 +0300

    Написал тесты под таску поиска пересечений https://online.sbis.ru/opendoc.html?guid=6a1896ba-57db-4fec-b603-626a2ca954e5

--- test/fixture/custompack/InterfaceModule1/amdModule.js
+++ test/fixture/custompack/InterfaceModule1/amdModule.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 define('InterfaceModule1/amdModule', ['css!InterfaceModule1/amdModule'], function() {
    return {
       _moduleName: 'InterfaceModule1/amdModule'
--- test/fixture/custompack/InterfaceModule2/amdModule.js
+++ test/fixture/custompack/InterfaceModule2/amdModule.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 define('InterfaceModule2/amdModule', ['css!InterfaceModule2/amdModule'], function() {
    return {
       _moduleName: 'InterfaceModule1/amdModule'
