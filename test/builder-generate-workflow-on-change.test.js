035c5d36d26eb6172da02796e77ac4f08c13f6e7
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2018-10-11 17:25:58 +0300

    Отключаем темизацию в 600 из-за сильно возросшего времени деплоя, нужно решать проблему через задание конфигурации сборки в рамках модуля, безусловная сборка для всех модулей это плохо https://online.sbis.ru/opendoc.html?guid=7694a49d-d0b8-45c6-bf12-cc3849f52ca6

--- test/builder-generate-workflow-on-change.test.js
+++ test/builder-generate-workflow-on-change.test.js
@@ -16,7 +16,7 @@ const workspaceFolder = path.join(__dirname, 'workspace'),
    moduleOutputFolder = path.join(outputFolder, 'Modul'),
    moduleSourceFolder = path.join(sourceFolder, 'Модуль');
 
-const { isSymlink, isRegularFile } = require('./lib');
+const { isSymlink } = require('./lib');
 
 const clearWorkspace = function() {
    return fs.remove(workspaceFolder);
@@ -87,7 +87,6 @@ describe('gulp/builder/generate-workflow-on-change.js', () => {
       resultsFiles.should.have.members([
          'ForRename_old.css',
          'ForRename_old.less',
-         'ForRename_old_online.css',
          'contents.js',
          'contents.json',
          'navigation-modules.json',
@@ -106,17 +105,14 @@ describe('gulp/builder/generate-workflow-on-change.js', () => {
       resultsFiles.should.have.members([
          'ForRename_old.css',
          'ForRename_old.less',
-         'ForRename_old_online.css',
          'ForRename_new.css',
          'ForRename_new.less',
-         'ForRename_new_online.css',
          'contents.js',
          'contents.json',
          'navigation-modules.json',
          'routes-info.json',
          'static_templates.json'
       ]);
-      (await isRegularFile(moduleOutputFolder, 'ForRename_new_online.css')).should.equal(true);
 
       // запустим таску повторно
       await runWorkflowBuild();
@@ -126,7 +122,6 @@ describe('gulp/builder/generate-workflow-on-change.js', () => {
       resultsFiles.should.have.members([
          'ForRename_new.css',
          'ForRename_new.less',
-         'ForRename_new_online.css',
          'contents.js',
          'contents.json',
          'navigation-modules.json',
