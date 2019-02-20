39171cd0fd476c2986ad038278321688abdbddf2
fedor_kolbeshin <fa.kolbeshin@tensor.ru>
2019-02-20 10:43:40 +0300

    throw an error for attempting to import private external modules into library and library private parts

---
+++ test/fixture/builder-generate-workflow/packLibraries/compiledCorrectResult/privateExternalDep.js
@@ -0,0 +1,9 @@
+define('Modul/privateExternalDep', [
+    'require',
+    'exports',
+    'Modul/_External_dependence/testExternalLibrary'
+], function (require, exports, testExternalLibrary_1) {
+    'use strict';
+    Object.defineProperty(exports, '__esModule', { value: true });
+    exports.default = testExternalLibrary_1.default;
+});
