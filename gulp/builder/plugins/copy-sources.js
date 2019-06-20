9924f85082b90e1fe51457636752c78c9f872167
fedor_kolbeshin <fa.kolbeshin@tensor.ru>
2019-06-20 14:34:53 +0300

    remove private parts of libraries from desktop application build

--- gulp/builder/plugins/copy-sources.js
+++ gulp/builder/plugins/copy-sources.js
@@ -15,7 +15,8 @@ const builderMeta = new Set([
    'routes-info.json',
    'static_templates.json'
 ]);
-
+const helpers = require('../../../lib/helpers');
+const path = require('path');
 const extensions = new Set([
    '.js',
    '.tmpl',
@@ -29,13 +30,43 @@ const extensions = new Set([
    '.ts',
    '.map'
 ]);
+const privateModuleExt = /(\.min)?(\.js|\.wml|\.tmpl)/;
+
+function getModuleNameWithPlugin(currentModule) {
+   const prettyFilePath = helpers.unixifyPath(currentModule.path);
+   const prettyRoot = helpers.unixifyPath(path.dirname(currentModule.base));
+   const prettyRelativePath = helpers.removeLeadingSlash(prettyFilePath.replace(prettyRoot, ''));
+   const currentModuleName = prettyRelativePath.replace(privateModuleExt, '');
+   const currentPlugin = currentModule.extname.slice(1, currentModule.extname.length);
+   const result = {
+      currentRelativePath: prettyRelativePath
+   };
+   switch (currentPlugin) {
+      case 'tmpl':
+      case 'wml':
+      case 'css':
+         result.normalizedModuleName = `${currentPlugin}!${currentModuleName}`;
+         break;
+      case 'xhtml':
+         result.normalizedModuleName = `html!${currentModuleName}`;
+         break;
+      default:
+         result.normalizedModuleName = currentModuleName;
+         break;
+   }
+   return result;
+}
 
 /**
  * Объявление плагина
  * @returns {stream}
  */
-module.exports = function declarePlugin(taskParameters) {
+module.exports = function declarePlugin(taskParameters, moduleInfo) {
    const buildConfig = taskParameters.config;
+   const modulesToCheck = [];
+   const currentModuleName = helpers.prettifyPath(moduleInfo.output).split('/').pop();
+   let versionedMetaFile, cdnMetaFile;
+
    return through.obj(
       function onTransform(file, encoding, callback) {
          /**
@@ -62,6 +93,16 @@ module.exports = function declarePlugin(taskParameters) {
             return;
          }
 
+         if (file.basename === 'versioned_modules.json') {
+            versionedMetaFile = file;
+            callback(null);
+            return;
+         }
+         if (file.basename === 'cdn_modules.json') {
+            cdnMetaFile = file;
+            callback(null);
+            return;
+         }
          if (!checkSourceNecessityByConfig(buildConfig, file.extname)) {
             callback(null);
             return;
@@ -76,7 +117,8 @@ module.exports = function declarePlugin(taskParameters) {
                 * пакет шаблон компонента 2 раза
                 */
                if (debugMode || isMinified || file.basename.endsWith('.min.original.js')) {
-                  callback(null, file);
+                  modulesToCheck.push(file);
+                  callback(null);
                   return;
                }
                callback(null);
@@ -100,11 +142,51 @@ module.exports = function declarePlugin(taskParameters) {
             // templates, .css, .jstpl, typescript sources, less
             default:
                if (debugMode || isMinified) {
-                  callback(null, file);
-                  return;
+                  modulesToCheck.push(file);
                }
                callback(null);
+               break;
          }
+      },
+
+      /* @this Stream */
+      function onFlush(callback) {
+         const moduleDeps = taskParameters.cache.getModuleDependencies();
+         const currentModulePrivateLibraries = new Set();
+         const modulesToRemoveFromMeta = new Set();
+         Object.keys(moduleDeps.packedLibraries).forEach((currentLibrary) => {
+            moduleDeps.packedLibraries[currentLibrary].forEach(
+               currentModule => currentModulePrivateLibraries.add(currentModule)
+            );
+         });
+         modulesToCheck.forEach((currentModule) => {
+            const { normalizedModuleName, currentRelativePath } = getModuleNameWithPlugin(currentModule);
+
+            // remove from gulp stream packed into libraries files
+            if (currentModulePrivateLibraries.has(normalizedModuleName)) {
+               modulesToRemoveFromMeta.add(currentRelativePath);
+               return;
+            }
+            this.push(currentModule);
+         });
+
+         // remove private parts of libraries from versioned and cdn meta
+         taskParameters.versionedModules[currentModuleName] = taskParameters.versionedModules[currentModuleName].filter(
+            currentPath => !modulesToRemoveFromMeta.has(currentPath)
+         );
+         taskParameters.cdnModules[currentModuleName] = taskParameters.cdnModules[currentModuleName].filter(
+            currentPath => !modulesToRemoveFromMeta.has(currentPath)
+         );
+         versionedMetaFile.contents = Buffer.from(JSON.stringify(
+            taskParameters.versionedModules[currentModuleName].sort()
+         ));
+         cdnMetaFile.contents = Buffer.from(JSON.stringify(
+            taskParameters.cdnModules[currentModuleName].sort()
+         ));
+         this.push(versionedMetaFile);
+         this.push(cdnMetaFile);
+
+         callback();
       }
    );
 };
---
+++ test/copy-sources.test.js
@@ -0,0 +1,137 @@
+'use strict';
+
+const initTest = require('./init-test');
+
+const path = require('path'),
+   fs = require('fs-extra');
+
+const generateWorkflow = require('../gulp/builder/generate-workflow.js');
+
+const { isRegularFile, linkPlatform } = require('./lib');
+
+const workspaceFolder = path.join(__dirname, 'workspace'),
+   cacheFolder = path.join(workspaceFolder, 'cache'),
+   outputFolder = path.join(workspaceFolder, 'output'),
+   sourceFolder = path.join(workspaceFolder, 'source'),
+   configPath = path.join(workspaceFolder, 'config.json'),
+   moduleOutputFolder = path.join(outputFolder, 'Modul');
+
+const clearWorkspace = function() {
+   return fs.remove(workspaceFolder);
+};
+
+const prepareTest = async function(fixtureFolder) {
+   await clearWorkspace();
+   await fs.ensureDir(sourceFolder);
+   await fs.copy(fixtureFolder, sourceFolder);
+};
+
+const runWorkflow = function() {
+   return new Promise((resolve, reject) => {
+      generateWorkflow([`--config="${configPath}"`])((error) => {
+         if (error) {
+            reject(error);
+         } else {
+            resolve();
+         }
+      });
+   });
+};
+
+describe('copy sources', () => {
+   before(async() => {
+      await initTest();
+   });
+
+   it('private parts of packed library, desktop app: should remove file from versioned and cdn meta, from output directory', async() => {
+      const fixtureFolder = path.join(__dirname, 'fixture/copy-sources/libraries-pack');
+      await prepareTest(fixtureFolder);
+      await linkPlatform(sourceFolder);
+
+      const config = {
+         cache: cacheFolder,
+         output: outputFolder,
+         typescript: true,
+         less: true,
+         themes: true,
+         wml: true,
+         minimize: true,
+         dependenciesGraph: true,
+         version: 'test',
+         sources: false,
+         modules: [
+            {
+               name: 'Модуль',
+               path: path.join(sourceFolder, 'Модуль'),
+            },
+            {
+               name: 'WS.Core',
+               path: path.join(sourceFolder, 'WS.Core'),
+               required: true
+            },
+            {
+               name: 'View',
+               path: path.join(sourceFolder, 'View'),
+               required: true
+            },
+            {
+               name: 'Vdom',
+               path: path.join(sourceFolder, 'Vdom'),
+               required: true
+            },
+            {
+               name: 'Router',
+               path: path.join(sourceFolder, 'Router'),
+               required: true
+            },
+            {
+               name: 'Application',
+               path: path.join(sourceFolder, 'Application'),
+               required: true
+            },
+            {
+               name: 'Inferno',
+               path: path.join(sourceFolder, 'Inferno'),
+               required: true
+            },
+            {
+               name: 'Env',
+               path: path.join(sourceFolder, 'Env'),
+               required: true
+            },
+            {
+               name: 'SbisEnv',
+               path: path.join(sourceFolder, 'SbisEnv'),
+               required: true
+            },
+            {
+               name: 'Browser',
+               path: path.join(sourceFolder, 'Browser'),
+               required: true
+            }
+         ]
+      };
+      await fs.writeJSON(configPath, config);
+
+      // запустим таску
+      await runWorkflow();
+
+      (await isRegularFile(moduleOutputFolder, 'library1.min.js')).should.equal(true);
+      (await isRegularFile(moduleOutputFolder, 'library1.js')).should.equal(false);
+
+      // all packed private parts of library should be remove from the output directory
+      (await isRegularFile(path.join(moduleOutputFolder, '_private'), 'module1.ts')).should.equal(false);
+      (await isRegularFile(path.join(moduleOutputFolder, '_private'), 'module1.js')).should.equal(false);
+      (await isRegularFile(path.join(moduleOutputFolder, '_private'), 'module1.min.js')).should.equal(false);
+      (await isRegularFile(path.join(moduleOutputFolder, '_private'), 'template1.tmpl')).should.equal(false);
+      (await isRegularFile(path.join(moduleOutputFolder, '_private'), 'template1.min.tmpl')).should.equal(false);
+
+      // check versioned and cdn meta for removed private parts of library
+      const versionedModulesMeta = await fs.readJson(path.join(moduleOutputFolder, '.builder/versioned_modules.json'));
+      const cdnModulesMeta = await fs.readJson(path.join(moduleOutputFolder, '.builder/cdn_modules.json'));
+
+      cdnModulesMeta.length.should.equal(0);
+      versionedModulesMeta.length.should.equal(0);
+      await clearWorkspace();
+   });
+});
