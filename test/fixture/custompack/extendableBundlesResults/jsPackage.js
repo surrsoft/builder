f5f1687b889b55376e3d0b750864937157bd1018
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2019-08-14 13:00:47 +0300

    fixed excluded css packer for the css with the same name as javascript module

--- test/fixture/custompack/extendableBundlesResults/jsPackage.js
+++ test/fixture/custompack/extendableBundlesResults/jsPackage.js
@@ -5,5 +5,5 @@ define("InterfaceModule1/amdModule",["css!InterfaceModule1/amdModule"],function(
 (function(){define('css!InterfaceModule2/moduleStyle',['css!InterfaceModule2/superbundle-for-builder-tests.package'],'');})();
 define("InterfaceModule2/amdModule",["css!InterfaceModule2/amdModule"],function(){return{_moduleName:"InterfaceModule1/amdModule"}});
 /* /InterfaceModule3/extend.package.json:superbundle-for-builder-tests.package.js */
-(function(){define('css!InterfaceModule3/amdModule',['css!InterfaceModule3/superbundle-for-builder-tests.package'],'');})();
 define("InterfaceModule3/amdModule",["css!InterfaceModule3/amdModule"],function(){return{_moduleName:"InterfaceModule1/amdModule"}});
+if(typeof window !== "undefined" && window.atob){define('css!InterfaceModule3/amdModule', function() {var style = document.createElement("style"),head = document.head || document.getElementsByTagName("head")[0];style.type = "text/css";style.setAttribute("data-vdomignore", "true");style.appendChild(document.createTextNode(".interfaceModule3_logoDefault{background-image:url(/resources/InterfaceModule3/images/logo-en.svg)}"));head.appendChild(style);});}
