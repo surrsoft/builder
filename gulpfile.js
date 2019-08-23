ab2a555e4d95ae5ade28d9591e2ebd5fc5de147c
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2019-08-23 17:00:36 +0300

    updated necessary node.js version

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,14 @@
 'use strict';
 
 const getLogsLevel = require('./lib/get-log-level');
+const semver = require('semver');
 
 try {
    // В самом начале проверим версию node. используем минимум возможностей node.js и ES6
-   if (process.versions.node.split('.')[0] < 8) {
+   if (!semver.satisfies(process.versions.node, '>=10.14.2')) {
       // не рискуем выводить через logger
       // eslint-disable-next-line no-console
-      console.log('[00:00:00] [ERROR] Для запуска требуется Node.js v8.0.0 или выше');
+      console.log('[00:00:00] [ERROR] Для запуска требуется Node.js v10.14.2 или выше');
       process.exit(1);
    }
 
