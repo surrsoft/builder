415a403f4e45fc5aef0bf67fc3c797e711e06b9c
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2020-01-29 13:08:27 +0300

    generate AMD-formatted meta for current module localization despite the fact of its content value. Will be completely remove in 20.2100 after completion of the task https://online.sbis.ru/opendoc.html?guid=0f5682ba-f95d-4bf9-a4b7-54a539e54c9d

--- test/builder-generate-workflow.test.js
+++ test/builder-generate-workflow.test.js
@@ -1166,6 +1166,7 @@ describe('gulp/builder/generate-workflow.js', () => {
       // проверим, что все нужные файлы появились в "стенде", лишние удалились
       resultsFiles = await fs.readdir(moduleOutputFolder);
       resultsFiles.should.have.members([
+         '.builder',
          'ForChange.js',
          'ForChange_new.html',
          'ForRename_new.js',
