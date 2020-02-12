2a184a01e1ee991e94538619954b40f03e9496cb
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2020-02-12 17:17:47 +0300

    We need to throw down build of current project on purpose to check is it an actual builder using with changes have taken instead of stable builder from SDK

--- gulp/builder/classes/configuration.js
+++ gulp/builder/classes/configuration.js
@@ -410,6 +410,10 @@ class BuildConfiguration {
        * 3) localization enabled.
        */
       this.initCore = this.needTemplates || this.builderTests || this.localizations.length > 0;
+
+      if (this.branchTests) {
+         throw new Error('this message gives us an information about successfully ruined build using testing builder. If you see this, congratulations!');
+      }
    }
 }
 
