0a9aafad4ca02c49c8c0b3ae127738607a7e5600
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2020-02-26 15:04:02 +0300

    an old functionality for additional meta of interface module localization content was removed from contents generating https://online.sbis.ru/opendoc.html?guid=683d9d9e-a2ae-4175-837e-88a55a4d89e6

--- gulp/builder/plugins/create-contents-json.js
+++ gulp/builder/plugins/create-contents-json.js
@@ -60,39 +60,9 @@ module.exports = function declarePlugin(taskParameters, moduleInfo) {
             });
             this.push(contentsJsFile);
             this.push(contentsJsonFile);
-            const
-               currentModuleName = helpers.prettifyPath(moduleInfo.output).split('/').pop(),
-               moduleMeta = moduleInfo.contents.modules[currentModuleName],
-               moduleMetaResult = {};
-
-            // in module.js meta i18n needs only "dict" property to use, store it if exists.
-            if (moduleMeta.hasOwnProperty('dict')) {
-               moduleMetaResult.dict = moduleMeta.dict;
-            }
-
-            /**
-             * generate AMD-formatted meta for current module localization despite the fact of its content value.
-             * Will be completely remove in 20.2100 after completion of the task
-             * https://online.sbis.ru/opendoc.html?guid=0f5682ba-f95d-4bf9-a4b7-54a539e54c9d
-             */
-            const moduleMetaContent = `define('${currentModuleName}/.builder/module',[],function(){return ${JSON.stringify(moduleMetaResult)};});`;
-            const moduleMetaFile = new Vinyl({
-               path: '.builder/module.js',
-               contents: Buffer.from(moduleMetaContent),
-               moduleInfo,
-               compiled: true
-            });
-            const moduleMetaMinFile = new Vinyl({
-               path: '.builder/module.min.js',
-               contents: Buffer.from(moduleMetaContent),
-               moduleInfo,
-               compiled: true
-            });
-            this.push(moduleMetaFile);
-            this.push(moduleMetaMinFile);
          } catch (error) {
             logger.error({
-               message: "Ошибка Builder'а",
+               message: 'Builder error',
                error,
                moduleInfo
             });
