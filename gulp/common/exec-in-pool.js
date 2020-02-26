7821605432582b7546366ac16a12a4bf25e627b4
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2020-02-26 18:29:55 +0300

    fixed critical errors processing from worker's pool https://online.sbis.ru/opendoc.html?guid=26459204-3682-4045-8e4b-19a239cee312

--- gulp/common/exec-in-pool.js
+++ gulp/common/exec-in-pool.js
@@ -7,14 +7,15 @@
 const logger = require('../../lib/logger').logger();
 
 /**
- * Выполнить фукнцию в пуле воркеров.
- * Установлен таймаут.
- * Подготавливаются данные для вывода корректных логов из пула воркеров.
- * @param {Pool} pool пул воркеров
- * @param {string} funcName имя функции, которую нужно выполнить в пуле воркеров
- * @param {Array} funcArgs аргументы функции, которую нужно выполнить в пуле воркеров
- * @param {string} filePath путь до обрабатываемого файла. Для красивых логов.
- * @param {ModuleInfo} moduleInfo информация о модуле. Для красивых логов.
+ * Execute function in worker's pool with timeout(6 minutes seconds by default)
+ * Also prepare data for correct transport of logs into gulp main process out of
+ * worker's pool.
+ * @param {Pool} pool - worker's pool
+ * @param {string} funcName - function to be executed in current worker's pool.
+ * @param {Array} funcArgs - arguments of function to be executed in current worker's pool.
+ * @param {string} filePath - full path of current processing file. Needed by logger.
+ * @param {ModuleInfo} moduleInfo - full information about current interface module. Needed by logger.
+ * @param {Number} workerTimeout - timeout for function execution.
  * @returns {Promise<[error, result]>}
  */
 async function execInPool(pool, funcName, funcArgs, filePath = '', moduleInfo = null, workerTimeout = 300000) {
@@ -31,6 +32,16 @@ async function execInPool(pool, funcName, funcArgs, filePath = '', moduleInfo = 
          .exec(funcName, [funcArgs, filePath, moduleInfoObj])
          .timeout(workerTimeout);
       logger.addMessagesFromWorker(messagesForReport);
+
+      /**
+       * Throw Gulp main process down if require error occurred to avoid logs hell of single-type messages
+       * about require errors. P.S. will be correctly caught by NodeJS process post-processing function
+       * in the end of gulp main process.
+        */
+      if (error && error.message.includes('node\'s require')) {
+         logger.error(error.stack);
+         process.exit(1);
+      }
       return [error, result];
    } catch (error) {
       return [error, null];
