b529490cc39a7a9134f24a229ed7c340ac995a9e
Колбешин Ф.А <fa.kolbeshin@tensor.ru>
2020-04-25 21:55:27 +0300

    added filter in new mark themed modules builder plugin https://online.sbis.ru/opendoc.html?guid=5d7552c9-b770-457a-9ccb-4da76ee15245 Also removed old deprecated info about themes from readme https://online.sbis.ru/opendoc.html?guid=cc87b127-b75d-42de-9b4b-0b24cae0a62d

--- gulp/builder/generate-task/mark-theme-modules.js
+++ gulp/builder/generate-task/mark-theme-modules.js
@@ -20,52 +20,51 @@ const startTask = require('../../common/start-task-with-timer');
  * @returns {Undertaker.TaskFunction}
  */
 function generateTaskForMarkThemeModules(taskParameters) {
-   if (!taskParameters.config.less) {
+   // analyse only interface modules supposed to have themes
+   const modulesWithThemes = taskParameters.config.modules
+      .filter(currentModule => currentModule.name.endsWith('-theme'));
+   if (!taskParameters.config.less || modulesWithThemes.length === 0) {
       return function skipMarkThemeModules(done) {
          done();
       };
    }
-   const tasks = taskParameters.config.modules
-
-      // analyse only interface modules supposed to have themes
-      .filter(currentModule => currentModule.name.endsWith('-theme'))
-      .map((moduleInfo) => {
-         const input = path.join(moduleInfo.path, '/**/_theme.less');
-         return function collectStyleThemes() {
-            return gulp
-               .src(input, { dot: false, nodir: true })
-               .pipe(
-                  plumber({
-                     errorHandler(err) {
-                        logger.error({
-                           message: 'Task markThemeModules was completed with error',
-                           error: err,
-                           moduleInfo
-                        });
-                        this.emit('end');
-                     }
-                  })
-               )
-               .pipe(mapStream((file, done) => {
-                  if (path.basename(file.path) === '_theme.less') {
-                     const currentModuleName = path.basename(moduleInfo.output);
-                     const currentModuleNameParts = currentModuleName.split('-');
-
-                     /**
-                      * Interface module name for new theme should always contains 3 parts:
-                      * 1)Interface module name for current theme
-                      * 2)Current theme name
-                      * 3) "theme" postfix
-                      * Other Interface modules will be ignored from new theme's processing
-                      */
-                     if (currentModuleNameParts.length > 2) {
-                        moduleInfo.newThemesModule = true;
-                     }
+   const tasks = modulesWithThemes.map((moduleInfo) => {
+      const input = path.join(moduleInfo.path, '/**/_theme.less');
+      return function collectStyleThemes() {
+         return gulp
+            .src(input, { dot: false, nodir: true })
+            .pipe(
+               plumber({
+                  errorHandler(err) {
+                     logger.error({
+                        message: 'Task markThemeModules was completed with error',
+                        error: err,
+                        moduleInfo
+                     });
+                     this.emit('end');
                   }
-                  done();
-               }));
-         };
-      });
+               })
+            )
+            .pipe(mapStream((file, done) => {
+               if (path.basename(file.path) === '_theme.less') {
+                  const currentModuleName = path.basename(moduleInfo.output);
+                  const currentModuleNameParts = currentModuleName.split('-');
+
+                  /**
+                   * Interface module name for new theme should always contains 3 parts:
+                   * 1)Interface module name for current theme
+                   * 2)Current theme name
+                   * 3) "theme" postfix
+                   * Other Interface modules will be ignored from new theme's processing
+                   */
+                  if (currentModuleNameParts.length > 2) {
+                     moduleInfo.newThemesModule = true;
+                  }
+               }
+               done();
+            }));
+      };
+   });
 
    const collectStyleThemes = startTask('markThemeModules', taskParameters);
    return gulp.series(
